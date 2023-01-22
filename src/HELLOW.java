7d6d5fd4cb6329c45b19db796e5541dd5f91e1bc
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-23 00:12:47 +0530

    noww we are the modifine the file

--- src/HELLOW.java
+++ src/HELLOW.java
@@ -5,7 +5,9 @@ public class HELLOW {
 		System.out.println("this is the my name shivam singh");
 // now we are the chack the int deta type
 		int value1 = 1;
+		int value2 = 456;
 		System.out.println(value1);
+		System.out.println("this is the name of the shivam and my roon number"+value2);
 	}
 
 }
