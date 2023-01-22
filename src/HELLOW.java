ab7929e3fc323e87b0163057ba0faa255052540c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-22 23:55:47 +0530

    now this is the changes

---
+++ src/HELLOW.java
@@ -0,0 +1,11 @@
+
+public class HELLOW {
+
+	public static void main(String[] args) {
+		System.out.println("this is the my name shivam singh");
+// now we are the chack the int deta type
+		int value1 = 1;
+		System.out.println(value1);
+	}
+
+}
