bb9f7452413255630bae81d92c91bc7b378c6fb1
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-23 19:38:47 +0530

    making parcentage finder

--- src/HELLOW.java
+++ src/HELLOW.java
@@ -8,6 +8,19 @@ public class HELLOW {
 		int value2 = 456;
 		System.out.println(value1);
 		System.out.println("this is the name of the shivam and my roon number"+value2);
+// now we know the int the maximum ddeta type 2147483648
+		// now we will the learn flot deta type  and also learn the boolen deta type 
+		float value3 = 5;
+		System.out.println("now we will the chack the float deta type"+value3);
+		// now we will chacke the double deta type 
+		double value5 = 75;
+		System.out.println("now wee will the print the double deta dype"+value5);
+		// now we are the divide the floaat variable the 
+		double value45 = 56d/34d;
+		System.out.print(value45);
+		// now we are the learn the bloun deta type and float deta type 
+		
+		
 	}
 
 }
