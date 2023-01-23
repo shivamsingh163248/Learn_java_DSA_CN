45fe8c0f85648f63a32ed6b6241d748b63d0a9d2
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-24 00:39:58 +0530

    leaarn the boolean deta type

---
+++ src/boolen.java
@@ -0,0 +1,32 @@
+import java.math.BigDecimal;
+import java.util.*;
+
+
+
+
+public class boolen {
+public static void main(String[] args) {
+	// now we are the test the boolen variable 
+	boolean var1 = false;
+	int v1;
+	Scanner vlauet = new Scanner(System.in);
+	System.out.println(var1);
+	// now we are the learn the variable of cherectere
+	// we re the using the unicode table
+	char var2 = '\u260E' ;
+	System.out.println(var2);
+	// now we are the use of the bloon variable 
+	double var5 = 6.89;
+	double var6 = 7.56545;
+	System.out.println(var5 + var6);
+	BigDecimal d1 = new BigDecimal("2.6505");
+	BigDecimal d2 = new BigDecimal("2.4565");
+	System.out.println(d1.add(d2));
+	
+	 v1 = vlauet.nextInt();
+	System.out.println(v1);
+	
+	vlauet.close();
+	
+}
+}
