cc53c5c4af13cfa7121043f27b12fce6c73fa0d1
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-24 13:30:56 +0530

    now we are the modifide

---
+++ src/TPYECASTING.java
@@ -0,0 +1,27 @@
+
+public class TPYECASTING {
+
+	public static void main(String[] args) {
+		// Type casting means that one seta type convert to another 
+		// now we will set the type casting example 
+		short x =5 ;
+		int y = x;
+		System.out.println(y);
+		// now java ides complaning the  for the int can not convert that to the short deta type 
+		int c = 6;
+		short g = (short)c;
+		System.out.println("here we are the print variable "+g);
+		//now we are the conver the explict type casting f;loat convert to double
+		double v2 = 20.674574768486;
+		float v3 = (float)v2;
+		System.out.println(v3);
+		String v6 = "my name is shivam singh ";
+				// now we are the changing the inplicit type casting are the avable heare 
+		// now we are the changing the the explicit type casting are the avable
+		int v8 = 8;
+		float v9 = v8;
+		System.out.println("now this is the my   girlfriend itisha birthday " +v6+v9);
+		
+	}
+
+}
