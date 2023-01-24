dbece764ae17431ca7979229ab3d1dc864d71aee
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-25 01:27:15 +0530

    add the equation

---
+++ src/equation.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class equation {
+public static void main(String[] args) {
+	// NOW We are the programing of the equation of the math matic of a+b whole square
+	float a,b,c ;
+	
+	Scanner input = new Scanner(System.in);
+	// now hear we are the taking input from the user 
+	// so we re the scanner function used and taking the input of the a 
+	System.out.println("enter the value of the a");
+	a = input.nextFloat();
+	System.out.println("enter the value of the b");
+	b = input.nextFloat();
+	c = (a*a)+(b*b)+(2*a*b);
+	System.out.println("the value of the (a+b)*(a+b) "+c);
+	input.close();
+	
+}
+}
