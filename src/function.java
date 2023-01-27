45fdde5a28f64ce87e062bd64fb67c767def4754
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-27 15:16:28 +0530

    add file

--- src/function.java
+++ src/function.java
@@ -23,6 +23,45 @@ public class function {
 		 // now we will change  and  type casting in java and double value convert into float
 		 System.out.println("multiplication  of the your value is  "+value3);
 	}
+	// we are the again write the divide function and use it 
+	static void divide(double value1 , double value2) {
+		// now we will the start program of the divided 
+		System.out.println("your the frist value is the devided "+value1);
+		System.out.println("your the second the value is the diviser "+value2);
+		// now we are the printing the value after calculating 
+		 double value4;
+		 value4 = value1/value2;
+		 int value3 = (int)value4;
+		 // now we will change  and  type casting in java and double value convert into float
+		 System.out.println("multiplication  of the your value is  "+value3);
+		
+	}
+	// now we are make  the function of the subtraction 
+	//hear we are the starting  the function 
+	static void subtraction (float value1 , float value2) {
+		// fist we are out put given of the input value 
+		float value3 = value1 - value2;
+		// now we are the printing the value 
+		System.out.println("the substraction value of the "+value3);
+	}
+	
+	// now again make the function of the modulus 
+	//write the function of the modulus 
+	// modulus function given the value of the remainder value after the division
+	
+	static void modulus(float value1 , float value2) {
+		//show the input from the user use the system out  function 
+		System.out.println("your the fist value is devidend "+value1);
+		System.out.println("your the second value is the diveser "+value2);
+		// now hear we the write the calculation part 
+		// the stored value in the next variable 
+		float value3 = value1%value2;
+		// use the method of the type casting and change the value float to integer
+		int value4 = (int)value3;
+		// now we will the print the value 
+		System.out.println("the modulus value of the "+value4);
+				}
+	
 	public static void main(String[] args) {
 		//today we are the learn the condition function with the help of the function
 		//now we are the write the function
