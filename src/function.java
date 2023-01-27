02575d7f305f91fce170e0f877b13dac6fd32d1b
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-27 15:15:12 +0530

    write code

---
+++ src/function.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class function {
+        // now we are the  write the function 
+	static void add(float value1 , float value2) {
+		// simple we write the add function 
+	 System.out.println("your frist value is the "+value1);
+	 System.out.println("your second value is  the "+value2);
+	 // now we are the value of the given integer 
+	 float value3;
+	 value3 = value1+value2;
+	 System.out.println("addation of the your value is  "+value3);
+	}
+	// now we are the again write the function of the multiply
+	// so we will now started the function of the multiply
+	static void multiply(double value1 , double value2){
+		// now hear we write the multiply program
+		System.out.println("your frist value is the "+value1);
+		 System.out.println("your second value is  the "+value2);
+		 // now we are the value of the given integer 
+		 double value4;
+		 value4 = value1*value2;
+		 float value3 = (float)value4;
+		 // now we will change  and  type casting in java and double value convert into float
+		 System.out.println("multiplication  of the your value is  "+value3);
+	}
+	public static void main(String[] args) {
+		//today we are the learn the condition function with the help of the function
+		//now we are the write the function
+
+	}
+
+}
