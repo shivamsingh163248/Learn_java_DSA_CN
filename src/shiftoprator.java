2fc00ab4eed55ef2dc7a8e47996f37226483fe80
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-31 00:06:40 +0530

    learn shift and ternary operators

---
+++ src/shiftoprator.java
@@ -0,0 +1,23 @@
+
+public class shiftoprator {
+
+	public static void main(String[] args) {
+		// now using the shift operators 
+		// we learn the shift operators in this program
+		System.out.println(10<<2);
+		// explain the work of the shift operators hear 
+		// 2 represent is the power  of the 2 and right 
+		//side the 10*2^2 = 10*4 =40 
+		// in the left shift oprators are the use of the multiply '
+		System.out.println(30<<4);
+		// explain the 30*2^4 
+		// now we are the using the right shift operators
+		System.out.println(10>>2);
+		// in the right shift operators method using of the divide 
+		System.out.println(30>>4);
+		// if we are the using the float value the we are the value of the execatly
+		
+
+	}
+
+}
---
+++ src/trinaryoperator.java
@@ -0,0 +1,15 @@
+
+public class trinaryoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ // here we will learn the tenary operator 
+		// what include in trinay operator  ? :
+		boolean x = (9>7)?true:false ;
+		System.out.println(x);
+		boolean y = (6>7)?true:false ;
+		System.out.println(y);
+				
+	}
+
+}
