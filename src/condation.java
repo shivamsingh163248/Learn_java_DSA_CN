39c98936b5f4e6fb536e959c8063bca20c8436a6
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-30 21:18:54 +0530

    changed

---
+++ src/condation.java
@@ -0,0 +1,31 @@
+
+public class condation {
+
+	public static void main(String[] args) {
+		// now we will learn the the the boolean data type and using the true and false opration 
+		boolean x = true;
+		boolean y = false ; 
+		if (true == false) {
+			System.out.println("this is the condation are the false i the true = false");
+			
+		}else {
+System.out.println("this is the condation or the else");
+	}
+// now here we will using the method of the logical operator of or and not and also usin the xor oprator 
+		
+// now we will describe the and or the operator usig the if else condition method 
+		if (true || false ) {
+			System.out.println("this is the condation of the or oprator");
+		}else {
+			System.out.println("this is the wrong condation ");
+		}
+		// testing again these condition using the use of the and operator 
+		
+	if (true && false) {
+		System.out.println("this is the condition of the true");
+	} else {
+  System.out.println("this is the condition of the false");
+  
+	}
+}
+}
