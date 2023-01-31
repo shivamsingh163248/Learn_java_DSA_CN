724dfd0bfb88a6daf674eea5f1d87a20a37951e4
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-01-31 14:57:10 +0530

    learn the assigment operators

---
+++ src/assmentoperators.java
@@ -0,0 +1,23 @@
+
+public class assmentoperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  // now we learn the assignment operators
+		int x = 5;
+		//write the assignment oprerators 
+		x += 5;//10 5+5 =10
+		System.out.println(x);
+		x -= 5 ; //10-5 =5 
+		System.out.println(x);
+		x *= 5 ; //5*5 = 25 
+		System.out.println(x);
+		x /= 5 ; //25/5 = 5
+		System.out.println(x);
+		// now we are the again use of modulus operators
+		x %= 5 ; // 5/5 = 0 remainder
+		System.out.println(x);
+		// now we see that the changed of the value of the x according to operation 
+	}
+
+}
