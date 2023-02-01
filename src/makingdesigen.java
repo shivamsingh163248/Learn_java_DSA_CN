dd1e1b58d1b3489daf84a5f15bbee91092fe4961
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-02-01 13:25:24 +0530

    design

---
+++ src/makingdesigen.java
@@ -0,0 +1,17 @@
+
+public class makingdesigen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// now we are the write the if else statement this is the conditional statement 
+		// check the if else program
+		int a = 18 ;
+		if (18<a) {
+			System.out.println("you are the eleigible for the voting  you have the more write");
+		} else {
+            System.out.print("you are not eligible for the voting ");
+		}
+
+	}
+
+}
