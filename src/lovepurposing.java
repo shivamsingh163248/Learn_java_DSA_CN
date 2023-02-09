aa5acca90e23fbb1e6b2a4904bfad05550709d1e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-02-09 23:45:00 +0530

    love program

---
+++ src/lovepurposing.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class lovepurposing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// now we will the writing the program of the purposing of the girlfriend
+		Scanner name = new Scanner(System.in);
+		// this input taking from the user 
+		System.out.println("enter your name");
+		String user_name = name.next();
+		System.out.println("enter your age");
+		int age = name.nextInt();
+		System.out.println("enter you gender example M/F");
+		char gender = name.next().charAt(0);
+		if ((age>18 && age<25)&& (gender == 'F')) {
+			System.out.println("i love you "+user_name);
+		} else {
+
+		}
+		
+
+	}
+
+}
