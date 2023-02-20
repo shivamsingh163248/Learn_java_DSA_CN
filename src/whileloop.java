a7e2f2536eb21d63de0cb2541b6ea11c1ae91fd9
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-02-21 01:09:19 +0530

    changed

---
+++ src/whileloop.java
@@ -0,0 +1,10 @@
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("now we are the printing the while program ");
+ 
+	}
+
+}
