0d0ec8af83d1500b8afe49bd3a4d62a531c4cdc1
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-03-10 04:14:28 +0530

    update

---
+++ src/breakcontinew.java
@@ -0,0 +1,35 @@
+import java.util.StringTokenizer;
+
+public class breakcontinew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// so we learn the in this video the break and continue statement 
+		// and learn about the break and continue statement 
+		// implementation 
+		for (int i = 0; i < 10; i++) {
+			// printing the one to 10 
+			if (i == 5) {
+				System.out.println("my nme is shivam and i testing the code");
+				
+				continue;
+			}
+			// we re the removing the 8 by the using break 
+			if (i != 7) {
+				System.out.print("this is the 7 numbeer ");
+				
+			}else {
+				System.out.print("this is not A 7 number");
+				break;
+			}
+			
+			
+			 System.out.println(i);
+			 // so i will try to printing the  table after removing 5
+			 // so now i learning the break and continue statement ;			 
+		
+		}
+
+	}
+
+}
