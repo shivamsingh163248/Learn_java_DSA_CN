9eb100275b11dc1e3204efab905d8440a4dae759
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-03-11 02:55:16 +0530

    shivam name code pattern

--- src/shivam.java
+++ src/shivam.java
@@ -1,62 +1,10 @@
-import java.util.Iterator;
-import java.util.StringTokenizer;
 
-public class breakcontinew {
+public class shivam {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// so we learn the in this video the break and continue statement 
-		// and learn about the break and continue statement 
-		// implementation 
-		for (int i = 0; i < 10; i++) {
-			// printing the one to 10 
-			if (i == 5) {
-				System.out.println("my nme is shivam and i testing the code");
-				
-				continue;
-			}
-			// we re the removing the 8 by the using break 
-			if (i != 7) {
-				System.out.println("this is the 7 numbeer ");
-				
-			}else {
-				System.out.println("this is not A 7 number");
-				break;
-			}
-			
-			
-			 System.out.println(i);
-			 // so i will try to printing the  table after removing 5
-			 // so now i learning the break and continue statement ;
-			
-		}
-		// now i will try to printing the triangle pattern 
-		for (int i = 0; i < 10; i++) {
-			for (int j = 10; j > i; j--) {
-				System.out.print(" ");
-			}
-			for (int j = 0; j < i; j++) {
-				
-				if (j == (i-1)|| j == 0) {
-					System.out.print(" *");
-					continue;
-				}
-				if (i==(10-1)) {
-					System.out.print(" a");
-					continue;
-				}
-				
-				
-				System.out.print("  ");
-				
-			}
-			System.out.println("");
-			
-		}
 		
-		
-		// so i will try the name printing of s
-		
+
 		for (int i = 0; i < 10 ; i++) {
 			// this loop are applied for the all loop 
 			for (int j = 0; j < 7; j++) {
@@ -69,7 +17,7 @@ public class breakcontinew {
 					continue;
 				}
 				
-				System.out.print(" *");
+				System.out.print("**");
 			}
 			// write the next world hear 
 			for (int j = 0; j < 7; j++) {
@@ -85,7 +33,7 @@ public class breakcontinew {
 			}
         	   
 				
-				System.out.print(" *");
+				System.out.print("**");
 				
 			}
            
@@ -106,7 +54,7 @@ public class breakcontinew {
 				continue;
 			 }
         	   
-			System.out.print(" *");
+			System.out.print("**");
 		}
            
            
@@ -156,7 +104,7 @@ public class breakcontinew {
         	  
 			
         	  
-			System.out.print(" *");
+			System.out.print("**");
 		} 
           
           
@@ -175,7 +123,7 @@ public class breakcontinew {
 				continue;
 			}
         	 
-			System.out.print(" *");
+			System.out.print("**");
 		}
          // now write the code of the space after the a 
          for (int j = 0; j < 7; j++) {
@@ -191,7 +139,7 @@ public class breakcontinew {
 			System.out.print("  ");
 			continue;
 			}
-			System.out.print(" *");
+			System.out.print("**");
 		}
          
 			
@@ -202,6 +150,7 @@ public class breakcontinew {
 			System.out.println(" ");
 			
 		}
+
 
 	}
 
