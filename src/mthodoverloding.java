20d67ca5987f52cc2e6a4b2bb94f9a1cad15f748
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-04-13 01:39:56 +0530

    changd

--- src/mthodoverloding.java
+++ src/mthodoverloding.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class mthodoverloding {
 
@@ -5,6 +6,7 @@ public class mthodoverloding {
 		// TODO Auto-generated method stub
 		// now call the function with the parameter 
 		shiva(5, 7);
+		gretestnum();
 
 	}
 public static void shiva(int a , int b ) {
@@ -14,5 +16,37 @@ public static void shiva(int a , int b ) {
 	int g = a+b ;
 	System.out.print(g);
 	
+}
+// again we will make the method of the find the greatest number of the input write the code for this 
+public static void gretestnum() {
+	// now we are the printing the output to display the user 
+	System.out.println("enter the frist number");
+	// make the input class 
+	Scanner input = new Scanner(System.in) ;
+	// now make the input is the scanner function 
+	int a = input.nextInt();
+	System.out.println("enter thE SECOND NUMBER ");
+	int b = input.nextInt();
+	System.out.println("enter the 3 number");
+	int c = input.nextInt();
+	System.out.println("enter thE 4 NUMBER ");
+	int d = input.nextInt();
+	System.out.println("enter the 5 number");
+	int e = input.nextInt();
+	int i = 0 ;
+	
+	// now we re the writing the condition
+	if (a>b) {
+		 i = a ;
+	} else if(b>a) {
+          i = b;
+	}else if (c > b) {
+			i = c ;
+    }else if (d > c) {
+    	i = d ;
+    }else if (e>d) {
+    	i = e ;
+    }
+	System.out.println(i);
 }
 }
