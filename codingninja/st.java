7a823421ebdee4ac826f03b98b6c898e1aa99974
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-04-19 10:50:33 +0530

    changed

---
+++ codingninja/st.java
@@ -0,0 +1,13 @@
+
+public class st {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// now we are learn the next lecture of the coding platform 
+		// we are the writing the program of the largest integer value 
+		int a = Integer.MAX_VALUE;
+		int b = Integer.MIN_VALUE;
+		System.out.println(b+" "+a);
+	}
+
+}
