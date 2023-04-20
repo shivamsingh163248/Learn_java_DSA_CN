d3c26f1290f7e24ef0ca386cf8b7e8d2927b5e22
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-04-20 07:02:59 +0530

    changed

--- codingninja/st.java
+++ codingninja/st.java
@@ -8,6 +8,14 @@ public class st {
 		int a = Integer.MAX_VALUE;
 		int b = Integer.MIN_VALUE;
 		System.out.println(b+" "+a);
+		
+		// we are the printing the asci value now we are the talking about the
+		char ch = 'a';
+		System.out.println('a'+10);
+		//also we are the character character also storing in the integer value so lets example 
+		int i = 'a'+3;
+		System.out.println(i);
+		
 	}
 
 }
---
+++ codingninja/typecasting.java
@@ -0,0 +1,30 @@
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ // mow we are the learn the type casting of the java 
+		float c = 34.32f;
+		// now are the try to change the in double to integer  value and also using the math function for the round of the value 
+		// here   we are   the using the expicitly type casting
+		System.out.println(c);
+		int g = (int)c;
+		System.out.println(g);
+		// using the math round function the round the value of the float and the double value 
+		System.out.println(Math.round(c));
+		// now again we  are the using the function for the the vlaue of the double value
+		// o we re the write the function the value 
+		double e = 23.556d;
+		// now are the printing the value of the double so we are the writing the program of the value of the double 
+		System.out.println(e);
+		System.out.println(Math.round(e));
+		// again we are the using the type casting method for the showing the result 
+		int j = (int)e;
+		// here the checking the value data are the lossed are not 
+		System.out.println(j);
+		
+		
+		
+	}
+
+}
