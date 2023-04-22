b3479b395e8a472d4f5908e19af124c46b59dc70
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-04-22 22:19:54 +0530

    power

---
+++ src/power.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		// now finding the size of the string 
+		// int lenght = n.length();
+		int even=0;
+		int odd =0;
+		while(n>0)
+		{
+			int lastDigit = n%10;
+			if(lastDigit%2==0)
+			{
+				even += lastDigit;
+			}
+			else
+			{
+				odd += lastDigit;
+			}
+
+			n = n/10;
+		}
+
+	System.out.println(even+" "+odd);
+	}
+
+}
