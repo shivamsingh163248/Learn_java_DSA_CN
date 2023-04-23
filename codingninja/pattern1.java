1a2edfb58a348b1e90b66b5aa21995f845a137e8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-04-23 22:35:36 +0530

    pattern 1

---
+++ codingninja/pattern1.java
@@ -0,0 +1,18 @@
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// now we are the writing program 
+		for (int i = 0; i < 4; i++) {
+			
+			for (int j = 0; j < 4; j++) {
+				System.out.print("*"+"");
+				
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
