02064799d4960f5c94eb9416a2bae5b0ea1c0a6d
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-04-27 09:39:47 +0530

    update bit wise oparetoe

---
+++ codingninja/xor_bit_wise_opreator.java
@@ -0,0 +1,12 @@
+
+public class xor_bit_wise_opreator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    int a=10,b=-20;
+		    System.out.print(a^b);
+		
+
+	}
+
+}
