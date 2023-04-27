d5a3777ae398c14bb4988d97f4e2925a8947a03b
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-04-27 21:39:08 +0530

    update

--- codingninja/xor_bit_wise_opreator.java
+++ codingninja/xor_bit_wise_opreator.java
@@ -8,7 +8,21 @@ public class xor_bit_wise_opreator {
 		    
 		        int a1 = 42;
 		        int b1 = ~a1; //(there is tilde sign before a)
-		        System.out.println(a1 + " " + b1);       
+		        System.out.println(a1 + " " + b1);      
+		        
+		        
+		           
+		            int x , y = 1;
+		            x = 10;
+		            if (x != 10 && x / 0 == 0)
+		                System.out.println(y);
+		            else
+		                System.out.println(++y);
+		       
+		            int x2 = 15;
+		            int y2 = x2++;
+		            int  z2 = ++x2;
+		            System.out.println(y2 +" " + z2);
 		    
 		
 
