eab0887bdd0e2d7498b0c97e94d51743cf1874f4
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-04-27 21:33:59 +0530

    bit wise oprator

--- codingninja/xor_bit_wise_opreator.java
+++ codingninja/xor_bit_wise_opreator.java
@@ -4,7 +4,12 @@ public class xor_bit_wise_opreator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		    int a=10,b=-20;
-		    System.out.print(a^b);
+		    System.out.println(a^b);
+		    
+		        int a1 = 42;
+		        int b1 = ~a1; //(there is tilde sign before a)
+		        System.out.println(a1 + " " + b1);       
+		    
 		
 
 	}
