29ee769eab05f1ea2a838713c1bd94afd0df6088
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-04-27 21:45:05 +0530

    update

--- codingninja/xor_bit_wise_opreator.java
+++ codingninja/xor_bit_wise_opreator.java
@@ -23,6 +23,27 @@ public class xor_bit_wise_opreator {
 		            int y2 = x2++;
 		            int  z2 = ++x2;
 		            System.out.println(y2 +" " + z2);
+		            
+		            
+		            
+		            int x4 =10;
+		            int y4 = 20;
+		            if(x4++ > 10 && ++y4 > 20 ){
+		            System.out.print("Inside if  ");
+		            }else{
+		            System.out.print("Inside else  ");
+		            }
+		            System.out.println(x4 +" "+y4);
+		            
+		            
+		            int x5 = 10;
+		            int y5 = 20;
+		            if(x5++ > 10 || ++y5 > 20 ){
+		           System.out.print("Inside if  ");
+		            }else{
+		           System.out.print("Inside else  ");
+		            }
+		            System.out.println(x5  + " " + y5);
 		    
 		
 
