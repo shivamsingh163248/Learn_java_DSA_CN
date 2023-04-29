d9654fbd58ce8a22d1637593e9e6852f71209dfe
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-04-29 14:38:12 +0530

    update

---
+++ codingninja/chackwhilelopp.java
@@ -0,0 +1,16 @@
+
+public class chackwhilelopp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		while(i < 5) {
+		    if(i == 3) {
+		        break;
+		    }
+		    System.out.print(i + " ");
+		    i++;
+		}
+	}
+
+}
