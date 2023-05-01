4261e30b4e878323e445d0f2afc721fadc81c7d0
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-01 19:20:30 +0530

    update

--- codingninja/chackwhilelopp.java
+++ codingninja/chackwhilelopp.java
@@ -6,24 +6,33 @@ public class chackwhilelopp {
 //		int i = 1;
 //		while(i < 5) {
 //		    if(i == 3) {
-//		        break;
+//		    	System.out.println("5");
+//		        continue;
 //		    }
 //		    System.out.print(i + " ");
 //		    i++;
 //		}
-		
+//		for (int j = 0; j < 5; j++) {
+//			 if(j == 3) {
+//			    	System.out.println("5");
+//			        continue;
+//			    }
+//			    System.out.print(j + " ");
+//			
 		int i = 1;
 		while(i < 3) {
-		    int j = 1;
+		    int j = 0;
 		    while(j < 5) {
+		        j++;
 		        if(j == 3) {
-		            break;
+		            continue;
 		        }
 		        System.out.print(j + " ");
-		        j++;
 		    }
 		    i++;
 		}
+		
+	
 	}
 
 }
