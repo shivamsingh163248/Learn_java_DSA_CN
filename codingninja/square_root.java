54e05a80cb5d5f8c61ac17bfaf6eb656ce4f871a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-03 20:06:42 +0530

    update

---
+++ codingninja/square_root.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class square_root {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner input = new Scanner(System.in);
+		  int n = input.nextInt();
+		  // now chack the condation nuber of the square is greater then or not of the given number
+		  int i = 0 ;
+		  int ans = 0;
+		  while (ans<= n ) {
+			  i++;
+			  System.out.println(i);
+			  ans = i*i;
+			  
+			 
+		  }
+		  System.out.print(i-1);
+
+	}
+
+}
