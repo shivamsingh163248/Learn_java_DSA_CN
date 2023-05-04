b96b7a6b8c565da0d922452851a5b2e50d5a9e3f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-05 04:36:07 +0530

    update

---
+++ milnestone2/powernumber.java
@@ -0,0 +1,21 @@
+
+public class powernumber {
+
+	
+		// TODO Auto-generated method stub
+		public static void func1(int a,int b)
+		{
+		    int ans=1;
+		    for(int i=0;i<b;i++)
+		    {
+		        ans*=a;
+		    }
+		    System.out.print(ans);
+		}
+		public static void main (String[] args) {
+		    func1(2,5);
+		}
+
+	}
+
+
