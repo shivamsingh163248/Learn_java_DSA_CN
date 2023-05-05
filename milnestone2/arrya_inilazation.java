d86f040e0a0026bba34c1de29643f5461cd5ebe2
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-06 00:03:16 +0530

    update

--- milnestone2/arrya_inilazation.java
+++ milnestone2/arrya_inilazation.java
@@ -1,4 +1,5 @@
 
+
 public class arrya_inilazation {
 
 	public static void main(String[] args) {
@@ -14,7 +15,16 @@ public class arrya_inilazation {
 		// now we are the direct printing the array 
 		System.out.println(shiv[0]);
 		System.out.println(shiv[1]);
-
+		// heare we are the learning the simple initialization  and declare the array
+		// now  again the write the code of the array  the 
+		int []shivam = new int [10];
+		int shivamm[];
+		// then now creating 
+		shivamm = new int [20];
+		shivamm[0] = 10;
+		shivamm[2] = 14 ; 
+		System.out.print(shivamm[0]+" "+shivamm[2]);
+		int aCarry ;
 	}
 
 }
