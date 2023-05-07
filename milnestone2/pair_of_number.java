b648f2869e5502df712f18807483f5cbb3e9f302
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-07 23:45:44 +0530

    update

---
+++ milnestone2/pair_of_number.java
@@ -0,0 +1,15 @@
+
+public class pair_of_number {
+	
+	// make the function of the take input from the user 
+	public static int[] take_input(){
+		// now creating the non primitive scanner type 
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
