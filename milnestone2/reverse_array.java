5bc5e25e562f56d6f0387da4c7159f572a2b32d4
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-13 17:21:47 +0530

    update

--- milnestone2/reverse_array.java
+++ milnestone2/reverse_array.java
@@ -23,6 +23,7 @@ public class reverse_array {
 		for (int i = 0; i < print.length; i++) {
 			System.out.print(print[i]+"  ");
 		}
+		System.out.println("");
 	}
    // public static void rotateright(int[]a ){
         // now find the array the lenght if the aray 
@@ -50,16 +51,42 @@ public class reverse_array {
     
   
    // }
-    public static void rotateright(int[]a ) {
+    public static void rotateright(int[]a ,int x ) {
     	
+    	// hear the calculate the size of the x 
+    	x = x%a.length;
     	int[]shiva = rotation(a, 0, a.length);
-    	a = shiva;
+    	print_array(shiva);
+    	
+    	
+    	int[]shivaa = rotation(shiva, 0, x);
+    	//a = shivaa ;
+    	print_array(shivaa);
+    	// now again reverse form insisial ndex to final index ;
+    	int[]reverse = rotation(shivaa, x, a.length);
+    	a = reverse ; 
+    	
+    }
+    // again making the function of the left rotation 
+    public static void leftrotation(int[]a , int x) {
+    	// fist the reverse array 
+    	x = x%a.length;
+    	int[]leftshiva = rotation(a, 0, a.length);
+    	print_array(leftshiva);
+    	int[]shivaaa = rotation(leftshiva, 0, a.length-x);
+    	//a = shivaa ;
+    	print_array(shivaaa);
+    	int[]reversei = rotation(shivaaa, a.length-x, a.length);
+    	print_array(reversei);
     	
     }
     
 	
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter the rotation size ");
+		int x = in.nextInt();
 		// TODO Auto-generated method stub
 		// now calling the function of the array 
 		int shiva[]= take_input();
@@ -67,8 +94,14 @@ public class reverse_array {
 		// now calling the function of the out put of the array 
 		print_array(shiva);
 		// passing in the reverse array 
-		rotateright(shiva);
-		print_array(shiva);
+		// right the code of the rotation take input from the user 
+//		
+//		rotateright(shiva ,x);
+//		print_array(shiva);
+		
+		System.out.println("left rorattion outout");
+		
+		leftrotation(shiva, x);
 
 	}
 
