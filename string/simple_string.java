ee777d7c13f7ddc9018161ac2eb62e967c10be18
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-15 08:57:54 +0530

    simple string testing

---
+++ string/simple_string.java
@@ -0,0 +1,14 @@
+
+public class simple_string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String i = "shivam";
+		System.out.println(i.charAt(0));
+		char j = i.charAt(1);
+		// so now [rinting the both charecter and test both are right or not 
+		System.out.println(j);
+
+	}
+
+}
