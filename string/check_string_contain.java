7986012ce8021197489cb2f684358a6353698bab
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-17 17:30:31 +0530

    changed

--- string/check_string_contain.java
+++ string/check_string_contain.java
@@ -9,11 +9,20 @@ public class check_string_contain {
 		// now compair between two string present or 
 		System.out.println(str.contains(stv));
 		// mow learn the check the append of the string 
-		// now creating the new string buffer 
-		StringBuffer sts = new StringBuffer("shivam");
+		// now creating the new string buffer
+		String name = "shivam";
+		StringBuffer sts = new StringBuffer(name);
 		sts.append("def");
-		// so hear we learn only string buffer are the use the append method and the setchar method  
+		// so hear we learn only string buffer are the use the append method and the setchar method
+		
+		// now changed in the string buffer
+		sts.setCharAt(1, ' ');
+System.out.println(sts);
 
+
+int j = 97 ;
+char g = (char)j;
+System.out.println(g);
 	}
 
 }
---
+++ string/d_array.java
@@ -0,0 +1,15 @@
+
+public class d_array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// creating the new array  fist coloum are the represent are the 
+		int[][]arr = new int[5][5];
+		// now store the value in the array 
+		arr[4][4] = 56 ; 
+		// now printing the array 
+		 System.out.println(arr[4][4]);
+		 System.out.println(arr[1][1]);
+	}
+
+}
