8d0847c3ac6667918cc4f18656c0a251ef77cbfa
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-19 14:19:57 +0530

    changed

--- string/star_boundary_pattern.java
+++ string/star_boundary_pattern.java
@@ -1,20 +1,123 @@
+import java.awt.Color;
+import java.util.Scanner;
 
 public class star_boundary_pattern {
+	// make the function of the taking input 
+	
+	public static int[][] take_input(){
+		Scanner input = new Scanner(System.in);
+		System.out.print("enter the size of the row ");
+		int row_size  = input.nextInt();
+
+		System.out.print(" enter the size of the column");
+		int column_size = input.nextInt();
+	
+		System.out.println("your matrix size "+row_size+"*"+column_size);
+		System.out.println("---------------------enter the matrix -------------------");
+		// ------------ creating the new array----------------------------
+		int[][]arr = new int[row_size][column_size];
+		// taking the input form the user 
+		
+		
+		
+		for (int i = 0; i < row_size; i++) {
+			for (int j = 0; j < column_size; j++) {
+				// ----------- user show index number------------
+				System.out.print("your  matrix index "+i+j+"=");
+				arr[i][j] = input.nextInt() ; 
+			}
+		}
+		
+		// ----------- return array ----------
+		return arr ; 
+	}
+	
+	// ---------------- making the new function of the output------------------------
+	public static void output(int[][]arr) {
+		System.out.println("------------------output of the matrix---------------------");
+		// find the length of the row column 
+		int row_size = arr.length;
+		int column_size = arr[0].length;
+		
+//		System.out.println(row_size);
+//		System.out.println(column_size);
+		for (int i = 0; i < row_size; i++) {
+			for (int j = 0; j < column_size; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			 System.out.println();
+		}
+		
+		
+	} 
+	
+	
+	// ------------------- make the new fnction ---------------------
+	public static void boundy(int[][]arr) {
+		
+		// -- find the size of the row and column ---------
+		int row_size = arr.length ; 
+		int column_size = arr[0].length;
+		
+		//----------- take the four pointer ----------------
+		
+		int row_big = 0 ;
+		int column_big = 0 ;
+		int row_end = row_size-1 ; 
+		int column_end = column_size-1;
+//		
+//		for (int i = row_big; i < row_end; i++) {
+//			System.out.print(arr[row_big][i]);
+//		}
+//		row_big++;
+//		System.out.println();
+//		
+//		for (int i = row_big; i < column_end; i++) {
+//			System.out.print(arr[i][row_end]);
+//		}
+//		row_end-- ;
+//		System.out.println();
+//		
+//		for (int i = column_big; i < row_end; i++) {
+//			System.out.print(arr[column_end][i]);
+//		}
+		for (int i = column_big; i <= column_end; i++) {
+			System.out.print(arr[column_big][i]+" ");
+		}
+		row_big++ ;
+		System.out.println();
+		for (int i = row_big; i <= column_end; i++) {
+			System.out.print(arr[i][column_end]+" ");
+		}
+		column_end-- ;
+		System.out.println();
+		for (int i = column_big; i <= column_end; i++) {
+			System.out.print(arr[row_end][i]+" ");
+		}
+		row_end -- ;
+		System.out.println();
+		for (int i = row_big; i <= row_end; i++) {
+			System.out.print(arr[i][column_big]+" ");
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		for (int i = 0; i < 4; i++) {
-			
-			for (int j = 0; j < 4; j++) {
-				if ((i>0 && i<3) && (j>0 && j<3)   ) {
-					System.out.print(" ");
-					continue ;
-				}
-				System.out.print("*");
-			}
-			System.out.println();
-			
-		}
+//------------ call function ------------------
+		 // creating the new array 
+		int[][]shiva ; 
+		shiva = take_input() ; 
+		output(shiva);
+		
+		//------- call the boundary function ---------
+		
+		boundy(shiva);
 
 	}
 
