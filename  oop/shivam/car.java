72ec2715e7572bc4838b0e597e23fdfd31ed0227
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-20 22:57:11 +0530

    changed

---  oop/shivam/car.java
+++  oop/shivam/car.java
@@ -1,9 +1,21 @@
-package shivam;
+ package shivam;
 
 public class car {
 
 	private String door ;
 	private String engine ;
 	private String driver ;
-	public String speed ;
+	private int speed ;
+	
+	 // now make the function of the use of 
+	public  void setspeeed(int speed) {
+		this.speed = speed ; 
+	}
+	// again make the function of of the return value 
+	public  int getspeed() {
+		return  speed ; 
+	}
+	
+	
+	
 }
---  oop/shivam/hello.java
+++  oop/shivam/hello.java
@@ -5,9 +5,10 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		car shi = new car();
-		shi.speed ="shivam";
-		System.out.println(shi.speed);
-
+		// now call the function from the next folder
+		shi.setspeeed(20);
+		shi.getspeed();
+		System.out.println(shi.getspeed());
 	}
 
 }
