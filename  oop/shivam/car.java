bf84002ab0571d22d69055ee1521d0f7bb4769f3
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-20 23:49:34 +0530

    changed

---  oop/shivam/car.java
+++  oop/shivam/car.java
@@ -9,13 +9,28 @@ public class car {
 	
 	 // now make the function of the use of 
 	public  void setspeeed(int speed) {
-		this.speed = speed ; 
+		this.speed = speed ;  //  getter and setter method 
 	}
 	// again make the function of of the return value 
 	public  int getspeed() {
 		return  speed ; 
 	}
-	
+	// now gain the creating the door 
+	public void setdoor(String door) {
+		this.door = door ; 
+	}
+	// for the printing using the getdoor 
+	public String getdoor() {
+		return door ; 
+	}
+	// again making the function of the  
+	public void setengin(String engin) {
+		this.engine = engin ;
+	}
+	// function calling  for the return of the return 
+	public String getengin() {
+		return engine ; 
+	}
 	
 	
 }
---  oop/shivam/hello.java
+++  oop/shivam/hello.java
@@ -9,6 +9,17 @@ public class hello {
 		shi.setspeeed(20);
 		shi.getspeed();
 		System.out.println(shi.getspeed());
+		
+		// call the value and the set and the check its working or not 
+		
+		shi.setdoor("black");
+		// now printing the door value which are the used to the make another object 
+		System.out.println(shi.getdoor());
+		
+		// again the set the value of the engin that are the used in the in the form of the object 
+		shi.setengin("2hm");
+		// printing value of the engin 
+		System.out.println(shi.getengin());
 	}
 
 }
