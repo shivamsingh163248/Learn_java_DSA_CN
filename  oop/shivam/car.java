63b193e3aa3bfed04cd69a013c23a18db3f72092
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-21 22:34:58 +0530

    changed

---  oop/shivam/car.java
+++  oop/shivam/car.java
@@ -5,9 +5,30 @@ public class car {
 	private String door ;
 	private String engine ;
 	private String driver ;
-	private int speed ;
+	public int speed ;
 	
-	 // now make the function of the use of 
+	
+	
+	
+	// now make the constructor 
+	public car() {
+		door = "open" ; 
+		engine = "1hz";
+		driver = "shivam";
+		speed = 6 ; 
+	}
+	
+	
+	 public car(String door, String engine, String driver, int speed) {
+		
+		this.door = door;
+		this.engine = engine;
+		this.driver = driver;
+		this.speed = speed;
+	}
+
+
+	// now make the function of the use of 
 	public  void setspeeed(int speed) {
 		this.speed = speed ;  //  getter and setter method 
 	}
@@ -30,6 +51,24 @@ public class car {
 	// function calling  for the return of the return 
 	public String getengin() {
 		return engine ; 
+	}
+	// also sets the parameter of the driver 
+	public void setdriver(String driver) {
+		
+		this.driver = driver;
+		
+	}
+	
+	
+	// make the function for the test theae function are in running state or not 
+	public String  carstate() {
+		if (door.equals("closed") && engine.equals("2hz") && speed == 30  ) {
+			 return "running" ;
+		}else {
+			 return "not running ";
+		}
+		//return "";
+		
 	}
 	
 	
---  oop/shivam/hello.java
+++  oop/shivam/hello.java
@@ -5,21 +5,31 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		car shi = new car();
-		// now call the function from the next folder
-		shi.setspeeed(20);
-		shi.getspeed();
-		System.out.println(shi.getspeed());
+////		 now call the function from the next folder'
+//	//	shi.speed = 20 ; 
+//		System.out.println(shi.speed);
+//		
+//	shi.setspeeed(30);
+//		shi.getspeed();
+//		System.out.println(shi.getspeed());
+//		System.out.println(shi.speed);
+//		
+//		// call the value and the set and the check its working or not 
+//		
+//		shi.setdoor("closed");
+//		// now printing the door value which are the used to the make another object 
+//		System.out.println(shi.getdoor());
+//		
+//		// again the set the value of the engin that are the used in the in the form of the object 
+//		shi.setengin("2hz");
+//		// printing value of the engin 
+//		System.out.println(shi.getengin());
+//		
+//		// again the call the function for the test car are  the running state or not 
 		
-		// call the value and the set and the check its working or not 
+		// directly call the over loading constructor call the
 		
-		shi.setdoor("black");
-		// now printing the door value which are the used to the make another object 
-		System.out.println(shi.getdoor());
-		
-		// again the set the value of the engin that are the used in the in the form of the object 
-		shi.setengin("2hm");
-		// printing value of the engin 
-		System.out.println(shi.getengin());
+		System.out.println(shi.carstate());
 	}
 
 }
