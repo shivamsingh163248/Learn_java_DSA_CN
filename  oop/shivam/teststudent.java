49283192643db09afe00cf25c23f9adbd2ff23f4
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-22 19:36:01 +0530

    update

---  oop/shivam/teststudent.java
+++  oop/shivam/teststudent.java
@@ -5,14 +5,18 @@ public class teststudent {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// now make the new object of the student 
-		student newbatch = new student() ; 
-		// creating the new object successful 
-		newbatch.class_name = 12 ; 
-		newbatch.id = 20569 ; 
-		newbatch.father_name = "satendra kumar singh ";
-		newbatch.mother_name = "sarita singh " ; 
-		
+//		student newbatch = new student() ; 
+//		// creating the new object successful 
+//		newbatch.class_name = 12 ; 
+//		newbatch.id = 20569 ; 
+//		newbatch.father_name = "satendra kumar singh ";
+//		newbatch.mother_name = "sarita singh " ; 
+//		
 		// after the set of the value  then return  the value 
+		
+		
+		// set the value directly and form the function 
+		student newbatch = new student(40, "satyeindra", 50, "sarita" , "mobile");
 		
 		System.out.println(newbatch.class_name);
 		System.out.println(newbatch.id);
