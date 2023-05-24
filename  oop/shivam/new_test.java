fac61c7dda82b5258d9a18dcc1972719cb52f91a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-24 10:04:37 +0530

    leran srtatic key word

---  oop/shivam/new_test.java
+++  oop/shivam/new_test.java
@@ -4,7 +4,15 @@ public class new_test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		test object = new test(); 
+		// set the value of the staic key word through the function 
+		
+		test shivam = new test() ; 
+		 // calling the function for the set the value 
+		shivam.shi(50);
+		// check the value 
+		// calling the function for the return the value 
+		shivam.shiva();
+		System.out.print(shivam.shiva());
 	}
 
 }
---  oop/shivam/test.java
+++  oop/shivam/test.java
@@ -1,37 +1,26 @@
 package shivam;
  class test {
 	
-	// now learning the const
-	 
-	 // now learning the final keyword that are the final value and again can not changed 
-	 
-//	
-//	    int a; 
-//	    System.out.println(this);
-	    
-	    // make the contructer for the insilazed the vlaue 
 
-	    
-	   public void  shivm( ) 
-	    {  
-	    	
-//	    	
-//	        this(10, 20);
-//	        System.out.println(this);
-//	        System.out.print("constructor one "); 
-	    	final int a  ; 
-	    }
-	    test(int a, int b) 
-	    { 
-	        this.a = a; 
-	       // check the printing the value of the number 
-	        System.out.println(this.b);
-	        System.out.print("constructor two ");
-	        
-	    } 
-	    
+// creating the static key world and using the inisalied the value of static key world 
+	 private String  name ; 
+	private  static int roll ;
+	 final int clas = 10  ;
+	 
+	// crating the function for the testing value in the function '
+	 void  shi(int a ) {
+		 this.roll = a ; 
+	 }
+	 // now learning the static function what is the static function and how it is the work 
+	   //again the set the value of the using the method of the getter and setter  
+	 // now usingh 
 	  
- 
+ // now return the value using the getter the function 
+	 public static  int shiva() {
+	//	 System.out.println(name); // when we wite the static keyword then you see the error of the non static 
+		                           // 
+		 return roll ; 
+	 }
 	  
 
 
