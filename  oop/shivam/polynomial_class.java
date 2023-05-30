04af23e196aa7fa5fd2170114a9cb5704d5fe1b3
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-30 09:06:50 +0530

    changed

---  oop/shivam/polynomial_class.java
+++  oop/shivam/polynomial_class.java
@@ -4,12 +4,12 @@ public class polynomial_class {
 	//  we are the using the access modifire 
 	
 	private int arr[] ;
-	
+	private  int size  ; 
 	// now creating the constructor with the no any parameter 
 	public polynomial_class() {
 		// select the size of the array 
 		arr = new int[5] ; 
-		
+		 size = 0 ;
 	}
 	
 	// creating the private double size function 
@@ -19,6 +19,68 @@ public class polynomial_class {
 		temp = arr ; // all the  reference  all the copy of the old array 
 		// now   creating the new array '
 		arr = new int [temp.length *2] ; 
+		for (int i = 0; i < temp.length; i++) {
+			arr[i] = temp[i] ; 
+		}
+	}
+	
+	// make the function using the gitter and setter for the checking the lengh of the arry and value of the array 
+	public void check_array() {
+		System.out.print("whole array is = " );
+		for (int i = 0; i < this.arr.length; i++) {
+			System.out.println(arr[i]+" ");
+		}
+	}
+	// now check the array  length 
+	public int array_size() {
+		int size = this.arr.length ; 
+		return size ; 
+	}
+
+	
+	// now creating the set value passed with the parameter 
+	public void set_cofficent(int cofficet , int degree) {
+		// now checking the degree are the exit or not in the array index 
+		if (arr.length -1 < degree) {
+			// calling the function of the double array 
+			double_size();
+		}
+		// store the value of the the array in the index 
+		// check the lenght of the array 
+		if (degree>=size) {
+			size = degree+1 ; 
+		}
+		
+		arr[degree] = cofficet ; 
+		
+		
+	}
+	
+	// again make the function of the for the printing 
+ // make public access modifier 
+	public void print_array_value() {
+		// now using the for loop for the printing of the array 
+		for (int i = 0; i < arr.length; i++) {
+			if (this.arr[i] != 0 ) {
+				System.out.print(this.arr[i]+"x"+i+" ");
+			}
+		}
+	}
+	
+	// creating the add function for the add two equation 
+	public polynomial_class add (polynomial_class s1) {
+		
+		// check the degeree are the same then add 
+		// check the size of the lenght 
+		int bigsize = Math.max(this.size, s1.size);
+		for (int i = 0; i < bigsize; i++) {
+			this.arr[i] = this.arr[i]+s1.arr[i];
+		}
+		// now creating the new array 
+		polynomial_class result = new polynomial_class() ; 
+		result.arr = this.arr ; 
+		return result ; 
+		
 	}
 	
 
---  oop/shivam/polynomial_main.java
+++  oop/shivam/polynomial_main.java
@@ -70,40 +70,62 @@ public class polynomial_main {
 		for (int i = 0; i < degree.length; i++) {
 			
 			// calling the function of the set value 
-			
+			shi.set_cofficent(cofficent[i], degree[i]);
 		}
 		
+		// agian creating the new object for the set the value of new cofficent and degree 
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		for (int i = 0; i < cofficent.length; i++) {
-			System.out.print(cofficent[i]+"x"+degree[i]+"+");
-			// if there same element on the polymomial then we are the input 
-			
+		 polynomial_class shi_next = new polynomial_class() ; 
+		 
+		 for (int i = 0; i < degeree_next_user_input.length; i++) {
+			shi_next.set_cofficent(cofficent_next_user_input[i], degeree_next_user_input[i]);
 		}
-		System.out.println();
-		for (int i = 0; i < degeree_next_user_input.length; i++) {
-			System.out.print(cofficent_next_user_input[i]+"x"+degeree_next_user_input[i]+"+");
-		}
+		 
+		 // now calling the adding function fist creating the new obgect 
+		  polynomial_class result  ; 
+		  
+		   result = shi.add(shi_next) ; 
+		   //check for the printing 
+		   result.print_array_value();
+		   
+		   // check  the print first value \
+		   System.out.println();
+		   // check the array size 
+		  System.out.println(shi.array_size());
+		   shi.print_array_value();
+		   System.out.println(shi_next.array_size());
+		   System.out.println();
+		   shi_next.print_array_value();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		for (int i = 0; i < cofficent.length; i++) {
+//			System.out.print(cofficent[i]+"x"+degree[i]+"+");
+//			// if there same element on the polymomial then we are the input 
+//			
+//		}
+//		System.out.println();
+//		for (int i = 0; i < degeree_next_user_input.length; i++) {
+//			System.out.print(cofficent_next_user_input[i]+"x"+degeree_next_user_input[i]+"+");
+//		}
 		
 
 	}
