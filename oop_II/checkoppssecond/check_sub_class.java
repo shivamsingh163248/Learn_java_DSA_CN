1432c3f8da04496c3ca165c90d5dc1ef959ea89f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-05-31 20:26:15 +0530

    changed

---
+++ oop_II/checkoppssecond/check_sub_class.java
@@ -0,0 +1,18 @@
+package checkoppssecond;
+
+import java_oops.classs;
+
+public class check_sub_class extends classs {
+
+	public check_sub_class(int roll_number, int teacher_id, String teacher_name, String teacher_subject,
+			int student_class, int total_student) {
+		super(roll_number, teacher_id, teacher_name, teacher_subject, student_class, total_student);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	
+	
+	
+}
