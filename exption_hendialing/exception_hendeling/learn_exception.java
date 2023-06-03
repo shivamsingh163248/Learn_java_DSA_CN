5548bee406859c2d8a7d13dc2fa3860e67fb651e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-03 20:10:06 +0530

    changed

--- exption_hendialing/exception_hendeling/learn_exception.java
+++ exption_hendialing/exception_hendeling/learn_exception.java
@@ -1,15 +1,18 @@
 package exception_hendeling;
 
+import java.util.Scanner;
+
 public class learn_exception {
 	
 	// creating the function of the array and string and arithmetic expression 
 	
 	// creating the function  of the array 
-	public  void check_array_eception (int number_of_time , int array_size) throws showError {
+	public static void check_array_eception (int number_of_time , int array_size) throws showError {
 		// using the for loop 
 		// creating the new array of the size n ; 
-		if (array_size > number_of_time) {
-			throw new ArrayIndexOutOfBoundsException() ; 
+		if (array_size < number_of_time) {
+			// throw new ArrayIndexOutOfBoundsException() ; 
+			throw new showError();
 		}
 		
 		// if am generating the own exception then creating the class for the generating the 
@@ -26,7 +29,7 @@ public class learn_exception {
 	
 	
 	// creating the next function of the string 
-	public int check_string(String name) throws showError  {
+	public static int check_string(String name) throws showError  {
 		
 	if (name == null) {
 		// creating the exception 
@@ -37,15 +40,73 @@ public class learn_exception {
 		return c ; 
 	}
 	
+	
+	
 	// again creating the divide function you know 0 cant not be divide any other value 
-public int divide(int a , int b ) {
+public static int divide(int a , int b ) throws showError {
+	if (b == 0) {
+		throw new showError() ; 
+	}
 	int c = a/b ; 
+	c = 10 ;
 	return c ; 
 	
 }
-	
 
-	public static void main(String[] args) {
+// creating one more function for the dealing with the exception 
+public static int check_divide_exception(int a , int b) {
+	int res = 0 ;
+	// call the above function 
+	
+	try {
+		res = divide(a, b) ;
+	} catch (showError e) {
+		// TODO Auto-generated catch block
+		System.out.println("hear we are the generating the exception ");
+		e.printStackTrace();
+	} 
+	return res ; 
+}
+//---------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------
+
+	public static void main(String[] args)  {
+		
+		// call the main function one by one and check the exception handling of the above function 
+		Scanner input = new Scanner(System.in) ; 
+		System.out.println("enter the size of the array ");
+		int array_size = input.nextInt() ; 
+		System.out.print("enter the loop of the array ");
+		int no_loop = input.nextInt() ; 
+		System.out.println("enter the string");
+		String name = input.next() ; 
+		System.out.println("enter the value for the divide");
+		int a  = input.nextInt() ; 
+		int b = input.nextInt() ; 
+		
+		// now calling the function 
+		try {
+			check_array_eception(no_loop, array_size);
+			System.out.println(" this the try of the loop ");
+		} catch (showError e1) {
+			// TODO Auto-generated catch block
+			System.out.println(" this is arry of the loop  this the catch function ");
+			e1.printStackTrace();
+		}
+		try {
+			check_string(name);
+			System.out.println(" this is for the string for the correct expression ");
+		} catch (showError e) {
+			// TODO Auto-generated catch block
+			System.out.println(" this the generatig the error fot the name ");
+			e.printStackTrace();
+		}
+		check_divide_exception(a, b);
+		
+		System.out.println(" this is for the overall ok ");
+		
+		
 		
 	}
 	
---
+++ recorsion/learn_recursion/recursion.java
@@ -0,0 +1,5 @@
+package learn_recursion;
+
+public class recursion {
+
+}
