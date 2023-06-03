da577483b80b48d96820abd6b1b5e85f9b49a7c1
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-03 21:42:30 +0530

    changed

--- recorsion/learn_recursion/recursion.java
+++ recorsion/learn_recursion/recursion.java
@@ -2,4 +2,18 @@ package learn_recursion;
 
 public class recursion {
 
+	private int c ; 
+	
+	// creating the function of the factoral 
+	
+	public void factorial(int n ) {
+		// find the valie of i less then factorial 
+		// check the condation for loop 
+		if (n== 0 ) {
+			return ; 
+		}
+	System.out.println( n );
+	factorial(n-1);
+	 
+	}
 }
---
+++ recorsion/learn_recursion/testrecursion.java
@@ -0,0 +1,13 @@
+package learn_recursion;
+
+public class testrecursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// creating the object 
+		recursion shi = new recursion() ; 
+		shi.factorial(5);
+
+	}
+
+}
