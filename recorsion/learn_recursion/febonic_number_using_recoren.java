5f7cd5ac6c9cac9c81ad1c89dbb2c0afa8e5c812
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-05 20:55:32 +0530

    changed

--- recorsion/learn_recursion/febonic_number_using_recoren.java
+++ recorsion/learn_recursion/febonic_number_using_recoren.java
@@ -1,9 +1,33 @@
 package learn_recursion;
 
 public class febonic_number_using_recoren {
+	// creating the function of the writing the febonenic number 
+	
+  public static int fabonic(int n ) {
+	  if (n == 1 || n==2) {
+		return 1 ; 
+	}
+	  
+	  // now calling the function each other and learn how actually work 
+	  System.out.println("value of n before "+n);
+	  int value1 = fabonic(n-1) ; 
+	  System.out.println("value 1 =" +value1 +" , n = "+ n );
+	  System.out.println("value of n  "+n);
+	  int value2 = fabonic(n-2) ; 
+	  System.out.println("value 2 =" +value2 +" , n = "+ n );
+	  
+	  
+	 // System.out.println(value1+" "+value2);
+	  int res = value1+value2 ; 
+	  return res ;
+  }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// calling the function 
+		int c = fabonic(5) ; 
+		System.out.println(c);
+		
 
 	}
 
