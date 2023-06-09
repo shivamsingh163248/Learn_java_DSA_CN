8b0946619b29d20d341be37c006e10d43df7daa2
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-09 22:20:22 +0530

    changed

---
+++ recorsion/learn_recursion_II/test_string_num.java
@@ -0,0 +1,13 @@
+package learn_recursion_II;
+
+public class test_string_num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char value = '4' - '0' ; 
+int res = value ; 
+System.out.println(res);
+	}
+
+}
