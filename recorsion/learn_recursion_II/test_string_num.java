9a8c7996be509aba299636f59ae289bfde360969
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-10 23:34:03 +0530

    changed

--- recorsion/learn_recursion_II/test_string_num.java
+++ recorsion/learn_recursion_II/test_string_num.java
@@ -1,6 +1,16 @@
 package learn_recursion_II;
 
+import java.util.Scanner;
+
 public class test_string_num {
+	
+	// creating the function of the for scanning number and given the integer 
+	public static int scanner(String massage) {
+		Scanner input = new Scanner(System.in) ; 
+		System.out.println(massage);
+		int x = input.nextInt() ; 
+		return x ; 
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -8,6 +18,15 @@ public class test_string_num {
 		char value = '4' - '0' ; 
 int res = value ; 
 System.out.println(res);
+
+System.out.println('\0');
+System.out.print(res);
+
+// check  the calling the function 
+int y = scanner("this the number") ; 
+System.out.println(y);
 	}
 
 }
+
+
