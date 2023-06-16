b45aed70afbc554c8f4cb0dae7834b7b1c0a0f84
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-16 23:11:05 +0530

    changed

--- spcaecoplexcity/learnspacecomplexicity/quickshort.java
+++ spcaecoplexcity/learnspacecomplexicity/quickshort.java
@@ -1,17 +1,68 @@
 package learnspacecomplexicity;
 
+import java.util.Scanner;
+
 public class quickshort {
 	
 	// meke the function of the partition 
+	public static int swepingQuickShort(int[]arr , int startingIndex , int LastIndex) {
+		// IF IAM TAKING THE POIET ELEMENT AT THE FRIST INEDEX 
+		int poivetElement = arr[startingIndex] ; 
+		// now find the point element at the right place 
+		int countelement =  0 ; 
+		// using the for loop for loop solving the problem 
+		for (int i = startingIndex+1; i <= LastIndex; i++) {
+			if (poivetElement > arr[i]) {
+				// then the count the value 
+				countelement++ ; 
+			}
+		}
+		
+		// now swetching the point element at the right positin 
+		int temp = arr[ startingIndex +  countelement] ; 
+		arr[ startingIndex +  countelement] = poivetElement ; 
+		arr[startingIndex] = temp ; 
+		
+		// now agian sweping the element using the while loop 
+		int  i = startingIndex ; 
+		int j = LastIndex ; 
+		// using while loop sweping are formed 
+		while (i<j) {
+			if (arr[i]< poivetElement) {
+				i++ ; 
+			}else if (arr[j]> poivetElement) {
+				j-- ; 
+			}else {
+				// now sweping  is the preform 
+				temp = arr[i] ; 
+				arr[i] = arr[j] ; 
+				arr[j] = temp ; 
+				i++ ; 
+				j-- ; 
+			}
+		}
+		
+		// now return the element the 
+		return startingIndex+countelement ; 
+		
+	}
 	
 	// mow make the quick short the function 
-	public static void quickshort(int[]arr , int startIngindex , int lastIndex) {
+	public static void quickshorts(int[]arr , int startIngindex , int lastIndex) {
 		// now write the base condition if this code 
 		if (startIngindex <= lastIndex) {
 			return  ; 
 		}
 		
 	// find the partition index and call the function 
+		int pointindex = swepingQuickShort(arr, startIngindex, lastIndex);
+		// now function call 
+		quickshorts(arr, 0, pointindex-1);
+		quickshorts(arr, pointindex+1, lastIndex);
+		
+		for (int i = 0; i < arr.length; i++) {
+		System.out.print(arr[i]+" ");
+		}
 		
 		
 		
@@ -19,6 +70,28 @@ public class quickshort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	// 	int[]arr = {31 ,56 ,20, 18, 27,3 ,4 ,8,10 , 30 , 40 } ;
+		  Scanner s = new Scanner(System.in);
+		        int size = s.nextInt(); 
+		        int[] input = new int[size];
+		        
+		        for(int i = 0; i < size; i++){
+		            input[i] = s.nextInt();
+		        }
+		     
+		    
+		
+		// find the array length 
+		int length = input.length ; 
+		quickshorts(input, 0, length-1);
+		
+		//sysout 
+//		for (int i = 0; i < input.length; i++) {
+//			System.out.print(input[i]+" ");
+//		}
+		
+		
+		
 
 	}
 
