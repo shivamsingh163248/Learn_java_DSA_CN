019991d0588d6e94a94dfbebbe9b8fb968b09d55
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-19 22:09:05 +0530

    changed

---
+++ opp-3/learn_opp_3/vehickal.java
@@ -0,0 +1,13 @@
+package learn_opp_3;
+
+public class vehickal {
+	// define the integer with the access modifier 
+	private String fule_type ; 
+	private String wheller ; 
+	private String company_name ; 
+	private int no_doors ; 
+	private String owner_name ; 
+	
+	
+	// creating the constructor 
+}
