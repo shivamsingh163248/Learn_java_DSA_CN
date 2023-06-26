84587663512b039bdf292321c6376213310e257d
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-26 21:14:17 +0530

    chnaged

--- opp-3/learn_abstract/bank.java
+++ opp-3/learn_abstract/bank.java
@@ -2,7 +2,7 @@ package learn_abstract;
 
 
 
-public class bank {
+public abstract  class bank {
 	// creating the some private variable 
 	private String user_name ; 
 	private String bank_name ; 
@@ -21,11 +21,15 @@ public class bank {
 	}
 	// creating the new constrocter 
 	public bank(String user_name, String bank_name, int account_number, String bramch_name) {
-		super();
-		this(user_name , bank_name) ; 
+		
+		this(user_name, bank_name) ; 
 		this.account_number = account_number;
 		this.bramch_name = bramch_name;
 	}
+	
+	// now creating the getter and setter method for the getting the value and for the setting the value 
+	// now creating the absteact method 
+	public abstract void atm() ; 
 
 
 }
---
+++ opp-3/learn_abstract/employ_detail.java
@@ -0,0 +1,5 @@
+package learn_abstract;
+
+public class employ_detail {
+
+}
