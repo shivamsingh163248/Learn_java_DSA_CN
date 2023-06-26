14e211583d29b679ded493d480ad83546166578f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-26 21:17:41 +0530

    changed

--- opp-3/learn_abstract/bank.java
+++ opp-3/learn_abstract/bank.java
@@ -29,7 +29,39 @@ public abstract  class bank {
 	
 	// now creating the getter and setter method for the getting the value and for the setting the value 
 	// now creating the absteact method 
-	public abstract void atm() ; 
+	public abstract void atm() ;
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getBank_name() {
+		return bank_name;
+	}
+	public void setBank_name(String bank_name) {
+		this.bank_name = bank_name;
+	}
+	public int getAccount_number() {
+		return account_number;
+	}
+	public void setAccount_number(int account_number) {
+		this.account_number = account_number;
+	}
+	public String getBramch_name() {
+		return bramch_name;
+	}
+	public void setBramch_name(String bramch_name) {
+		this.bramch_name = bramch_name;
+	} 
+	
+	// creaint the final method 
+	public final void shivam () {
+		System.out.println("this is the method final methoid ");
+	}
+	public void itisha() {
+		System.out.println("this is call by the another class ");
+	}
 
 
 }
--- opp-3/learn_abstract/employ_detail.java
+++ opp-3/learn_abstract/employ_detail.java
@@ -1,5 +1,53 @@
 package learn_abstract;
 
-public class employ_detail {
-
+public class employ_detail extends bank { // now extends the abstract class 
+ 
+	
+	// now hear the call the abstract class and full fill the property 
+	private int employ_number ; 
+	private String employ_departemnt ; 
+	private String designation ;
+	public employ_detail(int employ_number, String employ_departemnt, String designation) {
+		super(employ_departemnt);
+		this.employ_number = employ_number;
+		this.employ_departemnt = employ_departemnt;
+		this.designation = designation;
+	}
+	public int getEmploy_number() {
+		return employ_number;
+	}
+	public void setEmploy_number(int employ_number) {
+		this.employ_number = employ_number;
+	}
+	public String getEmploy_departemnt() {
+		return employ_departemnt;
+	}
+	public void setEmploy_departemnt(String employ_departemnt) {
+		this.employ_departemnt = employ_departemnt;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	} 
+	
+	// now creating the constrocter 
+	// creating the overide 
+	@Override
+		public void itisha() {
+			// TODO Auto-generated method stub
+			super.itisha();
+		}
+	// if an calling the final method this is the its is 
+	// calling the abstract method 
+	@Override
+		public void atm() {
+			// TODO Auto-generated method stub
+		System.out.println("yes you have the atm ");
+			
+		}
+	
+	
+	
 }
