2f0fd3d311e011fcc4b4a8c870a1dde70ff42f70
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-28 04:02:01 +0530

    chagned

--- opp-3/learn_generic/test_generic_method.java
+++ opp-3/learn_generic/test_generic_method.java
@@ -13,6 +13,25 @@ public class test_generic_method {
 		pair<String> itisha = new pair<String>("shivam", "itisha") ; 
 		// now creating the print fucntion for the check directly 
 		itisha.print();
+		
+		// now agian creaint the value for the set normally 
+	// 	pair p = new pair(20, 50) ; ///// it worng type method
+		
+		// now creating the generic class and how to use this method and apply with 
+		// generic method to learn the det the deta type accourding to you and the use according to you  
+		
+		pair_two_type<Integer, String> shi = new pair_two_type<Integer, String>(23, "itisha");
+		// now printing the 
+		shi.print();
+		// now again creating new object 
+		pair_two_type<pair_two_type<Integer, String> , String> iti = new pair_two_type<>();
+		iti.setShivam(shi);
+		iti.setItisha("love u itisha");
+		// printing the frist and second value of the object 
+		System.out.println(iti.getItisha());
+		System.out.println(iti.getShivam().getShivam());
+	System.out.println(iti.getShivam().getItisha());
+		
 
 	}
 
