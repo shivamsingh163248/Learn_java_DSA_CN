ff23882880f1affb68abbc95072eab805e4e3e6f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-27 21:24:13 +0530

    learn inteface and inheritenace

---
+++ opp-3/lear_inteface/bus.java
@@ -0,0 +1,8 @@
+package lear_inteface;
+
+public interface  bus {
+	// now agian creating the new interface class 
+	public abstract void bus_detail() ; 
+	public abstract int bus_number() ; 
+
+}
---
+++ opp-3/lear_inteface/total_main_class.java
@@ -0,0 +1,26 @@
+package lear_inteface;
+
+public class total_main_class  extends class_school implements bus , school { 
+
+	// calling the function of the class school 
+	@Override
+	void print() {
+		// TODO Auto-generated method stub
+		super.print();
+	}
+
+	@Override
+	public void bus_detail() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int bus_number() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	// now checking its working the work properly 
+	
+}
