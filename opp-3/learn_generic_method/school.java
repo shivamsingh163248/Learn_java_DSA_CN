0f53b0f393986755f0731c67bd664ae8bac4b32f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-28 11:13:54 +0530

    learn generic method

--- opp-3/learn_generic_method/school.java
+++ opp-3/learn_generic_method/school.java
@@ -1,5 +1,55 @@
 package learn_generic_method;
 
-public class school {
+public class school<itisha , shivam > {
+	// now learn the generic method 
 
+	
+	// write the some private varaible 
+	private itisha user_name ; 
+	private shivam roll_number ; 
+	
+	// now creating the constorctur 
+	public school() {
+		// TODO Auto-generated constructor stub
+	}
+	// again now creating the constocter 
+	public school(itisha user_name , shivam roll_number){
+		this.roll_number = roll_number ; 
+		this.user_name = user_name ; 
+		
+	}
+	public itisha getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(itisha user_name) {
+		this.user_name = user_name;
+	}
+	public shivam getRoll_number() {
+		return roll_number;
+	}
+	public void setRoll_number(shivam roll_number) {
+		this.roll_number = roll_number;
+	}
+	
+	// now creating the getter and setter method 
+	
+	// now creating method which 
+	public <itisha_singh> void print (itisha_singh shivam_singh ) {
+		System.out.println(shivam_singh);
+	}
+	// again creating the new function of the array of the return type 
+	public <itisha_shivam> itisha_shivam[] arry(itisha_shivam shivam[]){
+		
+		// now creating the new array 
+		// frist find the size of the array 
+		for (int i = 0; i < shivam.length; i++) {
+			// itisha_shivam itisha_shivam = shivam[i];
+			System.out.println(shivam[i]);
+			
+		}
+		
+		
+		return shivam  ; 
+	}
+	
 }
---
+++ opp-3/learn_generic_method/student_inteface.java
@@ -0,0 +1,10 @@
+package learn_generic_method;
+
+public interface student_inteface {
+	
+	// now creating the inteface method 
+	int shivam = 10 ; 
+	public abstract void studetn_name() ; 
+	public abstract int studetn_rollnumber() ; 
+
+}
