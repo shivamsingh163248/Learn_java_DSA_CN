1c42da3a56bf4b387d8d89d334205ead134cf708
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-06-29 21:54:25 +0530

    changed

--- opp-3/learn_generic_method/test_all_class.java
+++ opp-3/learn_generic_method/test_all_class.java
@@ -1,6 +1,16 @@
 package learn_generic_method;
 
 public class test_all_class {
+	
+	// now creating the printing  array  
+	public static <itisha> void printtt(itisha arr[]) {
+		// now using the for loop for the solving these question 
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -21,10 +31,24 @@ System.out.println(itisha.getUser_namee() +" "+ itisha.getRoll_number());
 
 // now calling the method of the 
 // creaitng the 
+// creating the new array 
+Integer arr[] = new Integer[5] ; 
+// now caliing the fubction of the arr
+
+// now se the valuie 
+for (int i = 0; i < arr.length; i++) {
+	arr[i] = i ;
+}
+ shivam.arry(arr);
+// calling the function 
+ printtt(arr);
+
  
 
-int[]arr = new int[6] ; 
-shivam.arry(arr);
+
+
+
+
 
 
 
