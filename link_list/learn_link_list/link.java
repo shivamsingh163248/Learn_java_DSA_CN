40cbf085370476ef6e74abf49f4ca64fac4e3527
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-01 03:17:43 +0530

    changed

---
+++ link_list/learn_link_list/link.java
@@ -0,0 +1,15 @@
+package learn_link_list;
+
+public class link {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// calling the function 
+		node<Integer>n1 = new node<Integer>(20) ; 
+		// now check the printing of the deta 
+		System.out.println(n1.data);
+		System.out.println(n1.next);
+
+	}
+
+}
---
+++ link_list/learn_link_list/node.java
@@ -0,0 +1,14 @@
+package learn_link_list;
+
+public class node<itisha> {
+
+	// creating the variabale of deata 
+	itisha data ; 
+	node<itisha> next ; 
+	// creating the constocter 
+	public node(itisha data) {
+		this.data = data ; 
+	}
+	
+	
+}
