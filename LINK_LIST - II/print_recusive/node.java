3dc4d59df39b83b6b5ef8633a1897bdcab18f623
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-07 22:30:49 +0530

    chabnged

---
+++ LINK_LIST - II/print_recusive/node.java
@@ -0,0 +1,14 @@
+package print_recusive;
+
+public class node <T> { // this is the using for the generic for the set the value according to you 
+  
+	// creating the the public integer value 
+	public T data ; 
+	// again creating the head value 
+	public node<T> next ; // this is the referencing to the object 
+	
+	// creating the node using the generic 
+	public void node(T data) {
+		this.data = data ; 
+	}
+}
