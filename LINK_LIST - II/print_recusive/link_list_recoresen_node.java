2c0cddd487313b6eaf3283507636da19a39aeab4
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-09 10:44:39 +0530

    learn recursin in the lnk list

--- LINK_LIST - II/print_recusive/link_list_recoresen_node.java
+++ LINK_LIST - II/print_recusive/link_list_recoresen_node.java
@@ -11,9 +11,33 @@ public class link_list_recoresen_node {
 		 Scanner input = new Scanner(System.in) ; 
 		 
 		 // now creating the take the input 
-		 int head = input.nextInt() ; 
+		 int data = input.nextInt() ; 
 		 
 		 // creating the loop function for the taking the input continusely 
+		 
+		 // taking the input in head 
+		 node<Integer> headNode = null , tail = null ; 
+		 
+		 // creating the while loop when data are the null 
+		 while (data != -1) {
+			
+			 // creating the if for the head node 
+			 // creating the new node 
+			 node<Integer>current_node = new node<Integer>(data); 
+			 if (headNode == null) {
+				// now set the value 
+				headNode = current_node ; 
+				tail = current_node ; 
+			}else {
+				tail.next = current_node ; 
+				tail = current_node ; 
+			}
+			 // creating the scanner function for the taking the input valu 
+			 data = input.nextInt() ; 
+			 
+		}
+		 // now hearing the return function 
+		 return headNode ; 
 		 
 	 }
 	
