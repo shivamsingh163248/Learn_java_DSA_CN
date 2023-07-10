7b103a693508d66a957f37cb64c561fee76a820c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-10 13:55:03 +0530

    changed

--- LINK_LIST - II/print_recusive/link_list_recoresen_node.java
+++ LINK_LIST - II/print_recusive/link_list_recoresen_node.java
@@ -41,8 +41,46 @@ public class link_list_recoresen_node {
 		 
 	 }
 	
+	 // again noe creating the printing function for the print the link list 
+	 
+	 public static void print(node<Integer>head) {
+		 // now writing the printing function 
+		 // taking the copy of the refrence value 
+		 node<Integer>temp = head ; 
+		 
+		 // creating the while loop for the printing the link list 
+		 while (temp != null  ) {
+		
+			 // creating the display for the output 
+			 System.out.println(temp.data);
+			 // now next adreess are in the current temp 
+			 temp = temp.next ;
+			
+		}
+	 }
+	 
+	 
+	 //  crreating the printing the printing function using the loop 
+	 public static void recurionprint(node<Integer>head) {
+		 // creating the base sunction 
+		 if (head == null) {
+			return ; 
+		}
+		 
+		 // callling the same function 
+		 recurionprint(head.next);
+		 // printing the for the display 
+		 System.out.println(head.data);
+	 }
 	
 	public static void main(String[] args) {
+		
+		// now calling the function 
+		node<Integer>takeinput = takeinput() ; 
+		print(takeinput);
+		// calling the recursion function for the printing 
+		System.out.println();
+		recurionprint(takeinput);
 		
 	}
 	
---
+++ LINK_LIST - II/takeinput_recursively/node.java
@@ -0,0 +1,17 @@
+package takeinput_recursively;
+
+public class node<T> { // creating the generic for the tekin variable according to you 
+	
+// creating the integer 
+	T data ; 
+	node<T> next ; // that are the storing the address ; 
+	
+	// creating the constocter 
+	
+	public node(T data) {
+		// TODO Auto-generated constructor stub
+		this.data = data ;
+	}
+	
+
+}
