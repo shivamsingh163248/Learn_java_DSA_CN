9dd7f620b60422ea523009294207f2415bce45a0
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-13 23:31:09 +0530

    learn mid point link list

---
+++ LINK_LIST - II/midpoint_linklist/node.java
@@ -0,0 +1,12 @@
+package midpoint_linklist;
+
+public class node <T>{ // creating the generic for the set thevalue according to you 
+	
+	T data ; 
+	node<T> next ; 
+	
+	// now creating the constructor for the set the value 
+	
+	
+
+}
