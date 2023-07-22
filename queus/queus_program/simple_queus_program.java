862cb43d2cf25019fcd36d2f3ec6efdcb97df1ee
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-23 01:42:05 +0530

    chagned

--- queus/queus_program/simple_queus_program.java
+++ queus/queus_program/simple_queus_program.java
@@ -1,5 +1,30 @@
 package queus_program;
 
-public class simple_queus_program {
 
+
+public class simple_queus_program {
+	
+	// creating the main fucntion for the writing the code of the ques 
+	
+	// now we are the creating the some data member 
+	private int front ; 
+	private int rear ;
+	private int[]data ; 
+	private int size ; 
+	
+	
+	// now creating the default constocter 
+	
+	
+	public simple_queus_program() {
+		// TODO Auto-generated constructor stub
+		// set the some basic value 
+		
+	}
+	
+		
+		
+		
+		
+	
 }
