bc11ea7780360fc3bfc95e7302ad26cfcca55a9d
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-25 00:03:17 +0530

    changed

--- queus/dyanmically_queu/queue.java
+++ queus/dyanmically_queu/queue.java
@@ -1,5 +1,42 @@
 package dyanmically_queu;
 
 public class queue {
+	
+	// fist we are the creating the member 
+	// it should  be the private access modifier 
+	private int[]data ; 
+	private int rear ; 
+	private int front ; 
+	private int size ; 
+	
+	
+	// now creating the parameter with 
+	public queue() {
+		// TODO Auto-generated constructor stub
+	// set the value 
+		data = new int[5];
+		rear  = -1; 
+		front = -1 ; 
+		
+		
+	}
+	public queue(int capacity ) {
+		// TODO Auto-generated constructor stub
+		data = new int [capacity] ; 
+		rear = -1 ; 
+		front = -1 ; 
+	}
+	
+	// now creating the function of the size 
+	public int size () {
+		return size  ; 
+	}
+	
+	public boolean Empty () {
+		return size == 0 ; 
+	}
+	
+	// creating the function of the inque 
+	
 
 }
