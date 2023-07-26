b1f5ffabe869e315a70d025e1f633e48469143bb
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-26 21:51:42 +0530

    learn queu collection

---
+++ queus/queuIncollection/queu.java
@@ -0,0 +1,35 @@
+package queuIncollection;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class queu {
+	
+	//importing the package 
+	
+	
+	
+	
+	
+	// creating the main method \
+	public static void main(String[] args) {
+		Queue<Integer> queuenum = new LinkedList<>();
+		
+		queuenum.add(23);
+		queuenum.add(34);
+		
+		// now printing the varable 
+		System.out.println(queuenum.peek());
+		System.out.println(queuenum.size());
+		System.out.println(queuenum.poll());
+		
+		
+	}
+	
+	
+	
+	
+	// now creating the add deta propery in this 
+	
+   
+}
