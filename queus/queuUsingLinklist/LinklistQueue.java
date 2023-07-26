e3fc73559da99a42c7c5040fbcabccdba78712ed
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-26 18:11:58 +0530

    learn

--- queus/queuUsingLinklist/LinklistQueue.java
+++ queus/queuUsingLinklist/LinklistQueue.java
@@ -1,5 +1,94 @@
 package queuUsingLinklist;
 
-public class LinklistQueue {
 
+
+
+
+public class LinklistQueue <T>{ // using the generic 
+
+	
+	// CREATING a deta type of the node 
+	// creating the both is the private access modifire 
+	
+	private node<T> front ; 
+	private node<T> rear ;
+	int size  ; 
+	
+	
+	// now creating the constructor 
+	
+	// creating the constocter
+	
+	LinklistQueue(){
+		// now set the deta of the front and rear 
+		
+		front =  null ; 
+	   rear =  null ; 
+	   size = 0 ; 
+	}
+	
+	// now creating the function of the isEMPTY 
+	
+	public boolean isEmpty () {
+		
+		return size == 0 ; 
+	}
+	
+	
+	// now creating the function of the size 
+	
+	public int size () {
+		return size ; 
+	}
+	
+	// now creating the function 
+	
+	public void inqueu (T ele) {
+		
+		// creating the new node elent se in the boject 
+		
+		// creating the size 
+		
+		/// creating the head and tail of the noe 
+		
+		// node<T> temp = null ; 
+		
+		
+		node<T>currentnode = new node<>(ele);
+		if (size == 0 ) {
+			front = currentnode ; 
+			rear = currentnode ; 
+		}
+		
+		// chck print of the current node
+		
+		System.out.println(currentnode);
+		
+		rear.next = currentnode ; 
+		rear = currentnode ; 
+		size++ ; 
+		
+		
+		
+		
+		// now creating the connection bwtween two node 
+//		temp  = front ; 
+//		
+//		temp.next = rear ; 
+//		temp = rear ; 
+//		
+	}
+	
+	// creating the front function 
+	
+	public node<T> front(){
+		return front ; 
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- queus/queuUsingLinklist/QueuUsingLinklist.java
+++ queus/queuUsingLinklist/QueuUsingLinklist.java
@@ -1,9 +1,45 @@
 package queuUsingLinklist;
 
 public class QueuUsingLinklist {
+	
+	// creating the function of the printing the linklist 
+	
+	public static  void print(node<Integer> head ) {
+		
+		// when queu is not null the printing the queu 
+		// creating the node 
+		node<Integer> temp  = head; 
+		
+	while (temp != null) {
+		// now printing the variable 
+		
+		System.out.print(temp.data+ "  ");
+		// creating the next to next 
+		
+		temp = temp.next ; 
+		
+	}
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// creating the object for the insurting thevalue in inque
+		
+		LinklistQueue<Integer>shivam = new LinklistQueue<>();
+		
+		// creating the in queu 
+		shivam.inqueu(25);
+		shivam.inqueu(26);
+		shivam.inqueu(27);
+		shivam.inqueu(28);
+		shivam.inqueu(29);
+		
+		// now creating the printing function 
+		
+		print(shivam.front());
+		
 
 	}
 
