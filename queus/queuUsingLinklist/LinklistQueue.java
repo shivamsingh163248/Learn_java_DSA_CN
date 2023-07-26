7b35b59532055e1a288df8a88c4e99ba3cbf4473
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-26 10:33:35 +0530

    changed

---
+++ queus/queuUsingLinklist/LinklistQueue.java
@@ -0,0 +1,5 @@
+package queuUsingLinklist;
+
+public class LinklistQueue {
+
+}
---
+++ queus/queuUsingLinklist/node.java
@@ -0,0 +1,16 @@
+package queuUsingLinklist;
+
+public class node <T> {
+	
+	// creating the node for the link list 
+	T data ; 
+	node<T> next ; 
+	
+	// creating the constocter  for the set the value of the deta  
+	 
+	public node(T data) {
+		// TODO Auto-generated constructor stub
+		this.data = data ; 
+	}
+
+}
