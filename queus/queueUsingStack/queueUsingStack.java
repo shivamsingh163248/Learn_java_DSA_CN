45bc0b7e68f649b5e1bacd8754412d07e5ee39d6
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-27 02:55:45 +0530

    changed

---
+++ queus/queueUsingStack/queueUsingStack.java
@@ -0,0 +1,41 @@
+package queueUsingStack;
+
+import java.util.Stack;
+
+import stackusing_library.stackcostoctreexample;
+
+public class queueUsingStack <T> {
+	
+	// creating the stack the of the program 
+	
+	
+	Stack<T> new1 ; 
+	Stack<T> new2 ; 
+	
+	// now we are the creating the constocter 
+	
+	public queueUsingStack() {
+		// TODO Auto-generated constructor stub
+		new1 = new Stack<>();
+		new2 = new Stack<>();
+	}
+	
+	
+	
+	// crreating the inque function 
+	 public void inque (T ele) {
+		 
+		 // creating the add method through the object 
+		 new1.add(ele) ; 
+		 
+	 }
+	 
+	 // now creating the deque function 
+	 
+	 public T dique () {
+		 
+		 
+		 
+	 }
+
+}
