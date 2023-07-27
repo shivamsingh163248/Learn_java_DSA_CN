d0901ddc96cc3599374c255724025933ec738e98
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-27 11:19:10 +0530

    learn 2 stack for the queue

--- queus/queueUsingStack/queueUsingStack.java
+++ queus/queueUsingStack/queueUsingStack.java
@@ -34,8 +34,34 @@ public class queueUsingStack <T> {
 	 
 	 public T dique () {
 		 
+		// now creating the loop when stack is wmpty 
 		 
+		 while (!new1.isEmpty()) {
+			
+			 // copy all the element of the stack new1 to switch ti new2 
+			 T temp = new1.pop() ; 
+			 // PEST ALL THE ELEMTN IN THE NEW STACK 
+			 new2.push(temp) ; 
+			 
+		}
 		 
+		 // remove the itme of the peek 
+		 T result = new2.peek();
+		 
+		 // again we are the reversing copying in the frist stack 
+		 
+		 // creating the while loop for the copy of the element 
+		 
+		 while (!new2.isEmpty()) {
+			
+			 // again creating the variable for the reversing copying the element 
+			 T tmep = new2.pop();
+			 // push in the frist stack agian 
+			 new1.push(tmep) ; 
+		}
+		 
+		// now the return element is 
+		 return result ; 
 	 }
 
 }
---
+++ queus/queueUsingStack/test2stackUsingQueu.java
@@ -0,0 +1,22 @@
+package queueUsingStack;
+
+public class test2stackUsingQueu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// no creating the object for the access the element 
+		queueUsingStack<Integer>shivam = new queueUsingStack<Integer>();
+		
+		// creating the add element in the stack 
+		shivam.inque(34);
+		shivam.inque(45);
+		shivam.inque(67);
+		
+		// check the printing of the dequeu element 
+		
+		System.out.println(shivam.dique());
+
+	}
+
+}
