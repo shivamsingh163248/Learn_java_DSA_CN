e1d741e7b8fa9b3aec5f20cea42672067238c1aa
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-29 03:18:32 +0530

    changed

--- opp-3/learn_generic_method/lerning_generic_method.java
+++ opp-3/learn_generic_method/lerning_generic_method.java
@@ -1,5 +1,49 @@
 package learn_generic_method;
 
+import takeinput_recursively.print_revers_recorsion;
+
 public class lerning_generic_method {
+	
+	// now creating the method of the printing the value 
+	public static <itisha> void print( itisha shivam []) {
+		// using the for each loop for the printing the number 
+		for (itisha name : shivam) {
+			// now prining the array 
+			
+			System.out.printf("%s ", name);
+			
+			
+		}
+		
+		
+	}
+	
+	// now creatig the for each llo
+	public static void prints(int[]arr) {
+		// printing the array 
+		for (int i : arr) {
+			System.out.println(i);
+		}
+		
+	}
+	
+	// creating the main function 
+	
+	public static void main(String[] args) {
+		
+		
+		
+		Integer arr[] = {2,4,5,6,7,56,6} ;
+		  int arrn[] = {2,4,5,6,7,56,6} ;
+	print(arr);
+	prints(arrn); 
+		
+		
+	}
+	
+	// creating the array 
+	
+	
+	// calling the function 
 
 }
