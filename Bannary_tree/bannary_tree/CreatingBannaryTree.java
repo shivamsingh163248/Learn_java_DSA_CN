837b7b3ef74ef73f375170473d7143caaf2f78d3
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-29 22:31:50 +0530

    changed

--- Bannary_tree/bannary_tree/CreatingBannaryTree.java
+++ Bannary_tree/bannary_tree/CreatingBannaryTree.java
@@ -4,6 +4,21 @@ public class CreatingBannaryTree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// creating the object of the node that is the root node 
+		BinnaryTreeNode<Integer>root = new  BinnaryTreeNode<Integer>(20) ; 
+		// creating the two more node one the left node and the second one the right node 
+		// again creating the object and creating the both node as the set of the 
+		// Binary tree 
+		BinnaryTreeNode<Integer>LeftNode = new BinnaryTreeNode<Integer>(45) ; 
+		// agian creaing the right node 
+		BinnaryTreeNode<Integer>RightNode = new BinnaryTreeNode<Integer>(234) ; 
+		
+		// now creating the linking procress 
+		
+		root.LeftNde = LeftNode ; 
+		root.RigtNode = RightNode ; 
+		
 
 	}
 
