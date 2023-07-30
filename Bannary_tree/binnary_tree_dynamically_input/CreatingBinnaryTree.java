632f7e858aaaed301d9e474fccaf1a9063ee8796
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-07-30 14:09:23 +0530

    learn input of binary treee

--- Bannary_tree/binnary_tree_dynamically_input/CreatingBinnaryTree.java
+++ Bannary_tree/binnary_tree_dynamically_input/CreatingBinnaryTree.java
@@ -1,5 +1,7 @@
 package binnary_tree_dynamically_input;
 
+import java.util.Scanner;
+
 import bannary_tree.BinnaryTreeNode;
 
 public class CreatingBinnaryTree {
@@ -44,6 +46,45 @@ public class CreatingBinnaryTree {
 		
 		// now again creating the value 
 	// 	System.out.println();
+		
+		
+		
+	}
+	
+	// now creating the takeinput function for the binnary tree take input 
+	
+	public static BinaryTreeNode<Integer> takeinputBinarytree(){
+		
+		// print the enter the root 
+		System.out.println("enter the root : ");
+		// now creating the scanner object 
+		Scanner input = new Scanner(System.in) ; 
+		
+		// now taking the input in the form of the integer 
+		int rootdata = input.nextInt();
+		
+		// checking the base condition 
+		// if the 
+		if (rootdata == -1 ) {
+			
+			// then 
+			return  null ;
+			
+		}
+		
+		// now creating the object of the 
+		BinaryTreeNode<Integer>root = new BinaryTreeNode<Integer>(rootdata) ; 
+		
+		// now using the recusion 
+		 BinaryTreeNode<Integer>leftnode = takeinputBinarytree() ; 
+		 BinaryTreeNode<Integer>rightnode = takeinputBinarytree() ; 
+		 
+		 // now linking process 
+		 
+		 root.LeftNode = leftnode ; 
+		 root.RightNode = rightnode ; 
+		 
+		 return root  ; 
 		
 		
 		
