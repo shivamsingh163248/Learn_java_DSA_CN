7e70cf174b861c49f48364af6ee774e906ddc949
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-08-03 01:05:10 +0530

    making binary node

--- binary_tree_II/BinaryTree/BinaryTreeNode.java
+++ binary_tree_II/BinaryTree/BinaryTreeNode.java
@@ -1,5 +1,21 @@
 package BinaryTree;
 
-public class BinaryTreeNode {
+public class BinaryTreeNode <T>{ // CREATING THE 	generic 
 
+	// NOW creating the deta 
+	 public T data ; 
+	
+	// now creating the node refrence 
+	
+public 	BinaryTreeNode<T>LeftNode ; 
+public	BinaryTreeNode<T>RightNode ; 
+	
+	// now creatng the constocter
+ public BinaryTreeNode (T data ) {
+	 // now set the value 
+	 this.data = data ; 
+	 
+ }
+	
+	
 }
