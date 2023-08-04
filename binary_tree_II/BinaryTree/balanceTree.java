e11e77af62a236cbe391c84792e5df8f9a506ed4
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-08-04 22:06:40 +0530

    creating the function of the hight

--- binary_tree_II/BinaryTree/balanceTree.java
+++ binary_tree_II/BinaryTree/balanceTree.java
@@ -1,6 +1,33 @@
 package BinaryTree;
 
 public class balanceTree {
+	
+	// creating the function of the hight of the tree 
+	
+	public static int HightBinaryTree(BinaryTreeNode<Integer>root) {
+		// creating the base condition 
+		if (root == null ) {
+			return 0 ; 
+		}
+		
+		
+		// calling the function 
+		
+		int left = HightBinaryTree(root.LeftNode) ; 
+		int right = HightBinaryTree(root.RightNode) ; 
+		
+		
+		// check the conditon of te return 
+		if (left>right) {
+			return 1+ left ; 
+		}else {
+			return 1+right ; 
+		}
+		
+		
+			}
+	
+	// creating the function og thebalanceing 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
