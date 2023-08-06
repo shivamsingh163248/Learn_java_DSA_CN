33cf3571c89b9f1571e42f2ac2f20f7f84bef5cf
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-08-06 22:12:09 +0530

    changed

--- binary_tree_II/find_maximun_minum_using_pair/FindMaxMin.java
+++ binary_tree_II/find_maximun_minum_using_pair/FindMaxMin.java
@@ -1,5 +1,8 @@
 package find_maximun_minum_using_pair;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class FindMaxMin {
 	
 	// creating the class of the pair 
@@ -49,6 +52,64 @@ public class FindMaxMin {
 		  // now calling for the left and right 
 		  getMinAndMaxHelper(root.LeftNode);
 		  getMinAndMaxHelper(root.RightNode);
+		  
+	  }
+	  
+	  
+	  
+	  // now gain creating the lavel wise pritning 
+	  public static void LavelWisePrinting(BinaryTreeNode<Integer>root) {
+		  
+		  // now creating the base condtion for the lavel wise printing 
+		  if (root == null ) {
+			return ; 
+		}
+		  
+		  // now creating the object of the  queue 
+		  Queue<BinaryTreeNode<Integer>>pendingnode = new LinkedList<BinaryTreeNode<Integer>>();
+		  //now add the  root element in 
+		  pendingnode.add(root) ; 
+		  // now adding the null in the queue for the space printing 
+		  pendingnode.add(null) ; 
+		  
+		  
+		  // now creating the loop 
+		  while (!pendingnode.isEmpty()) {
+			
+			  // now creating the condtion 
+			  // creating the rtefrence of the node 
+			  BinaryTreeNode<Integer>front = pendingnode.poll();
+			  
+			  // now checking the condition 
+			  if (front == null) {
+				
+				  if (pendingnode.isEmpty()) {
+					break ; 
+				}else {
+					System.out.println();
+					// now add null ofr the next term 
+					pendingnode.add(null) ; 
+				}
+				  
+				  
+				  
+			}else {
+				
+				// printing for the left ans right 
+				System.out.print(front.data+" ");
+				
+				//  name = new ();
+				if (front.LeftNode != null) {
+					// now adding the deta of the left 
+					pendingnode.add(front.LeftNode) ; 
+				}
+				if (front.RightNode != null) {
+					// now adding the deta of the left 
+					pendingnode.add(front.RightNode) ; 
+				}
+			}
+		}
+		  
 		  
 	  }
 	
