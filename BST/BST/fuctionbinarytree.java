1c24662f70bda5d547e1bd67a32c5e506310cedd
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-08-27 14:58:20 +0530

    changed

---
+++ BST/BST/fuctionbinarytree.java
@@ -0,0 +1,30 @@
+package BST;
+
+public class fuctionbinarytree {
+	
+	// creating the all function of the node 
+	// creating the take function ]
+	// 	creating the function of the teching the input 
+	
+	
+	public BinnarySearchTreeNode<Integer>takeinput(boolean isRoot , int Present , boolean rootleft){
+		
+		// creating the condition 
+		if (isRoot) {
+			System.out.println("enter the root : ");
+		}else if(rootleft) {
+			System.out.println("enter the leftnode : "+Present);
+		}else {
+			System.out.println("enter the rightndoe : "+Present);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
