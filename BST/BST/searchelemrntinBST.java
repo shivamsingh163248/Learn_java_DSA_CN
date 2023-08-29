bbad75fe278addf13c94a0eebfc2471b7ee93207
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-08-29 23:59:12 +0530

    commit

--- BST/BST/searchelemrntinBST.java
+++ BST/BST/searchelemrntinBST.java
@@ -1,5 +1,18 @@
 package BST;
 
 public class searchelemrntinBST {
+	
+	// CREATING THE three function 
+	// level wise input 
+	// level wise output 
+	// banary searach tree 
+	
+	// creating the function for the take input level wise 
+	public BinnarySearchTreeNode<Integer> takeinput_levelwise(){
+		
+		// creating the simple function using the queue 
+		 // write the function of the takeinput lavelwise
+		
+	}
 
 }
