559ecbfbf1fdc689bdc399619aa47aaee02b4bb4
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-08-30 13:08:08 +0530

    changed

--- BST/BST/searchelemrntinBST.java
+++ BST/BST/searchelemrntinBST.java
@@ -1,5 +1,9 @@
 package BST;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class searchelemrntinBST {
 	
 	// CREATING THE three function 
@@ -12,6 +16,28 @@ public class searchelemrntinBST {
 		
 		// creating the simple function using the queue 
 		 // write the function of the takeinput lavelwise
+		
+		Scanner input = new Scanner(System.in) ; 
+		
+		// creating the display for the user for the 
+		System.out.println("enter the root = ");
+		// now take the input in the variable 
+		int rootData = input.nextInt();
+		
+		
+		
+		// now creating the condition if the user input -1 then 
+		if (root == -1) {
+			return null ; 
+		}
+		
+		// now creating the node for the root data creating the node fot the storing the input in the value 
+		 
+		BinnarySearchTreeNode<Integer>node_root = new BinnarySearchTreeNode<Integer>(rootData) ; 
+		
+		// creating the  queue using the linklist 
+	Queue<BinnarySearchTreeNode<Integer>> pendingelement = new LinkedList<BinnarySearchTreeNode<Integer>>() ; 
+	
 		
 	}
 
