53d67a566b4598dd0e1176a3c72f70181c742a08
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-09-02 21:45:25 +0530

    changed

--- BST/BST/searchelemrntinBST.java
+++ BST/BST/searchelemrntinBST.java
@@ -38,6 +38,32 @@ public class searchelemrntinBST {
 		// creating the  queue using the linklist 
 	Queue<BinnarySearchTreeNode<Integer>> pendingelement = new LinkedList<BinnarySearchTreeNode<Integer>>() ; 
 	
+	    // creating the add refrence of the node 
+	pendingelement.add(node_root);
+	
+	// creating the condtion of the loop 
+	
+	while (!pendingelement.isEmpty()) {
+		 
+		//now storing data from the poll 
+		BinnarySearchTreeNode<Integer>front = pendingelement.poll();
+		// mow take the input from the 
+		System.out.println("enter the left node : "+front.data);
+		int leftnode = input.nextInt() ; 
+		
+		
+		// now check the condtion og the lft node id the empty or not 
+		
+		if (leftnode != -1 ) {
+			
+			// now pushing the element of the node 
+			// now creating the new node 
+			
+		}
+		
+		
+		
+	}
 		
 	}
 
