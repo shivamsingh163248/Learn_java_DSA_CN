0b4ab670b0ac59af0d6a0f861348918093fd32a9
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-09-20 23:25:11 +0530

    update

--- BST/BST/bst_simple_search.java
+++ BST/BST/bst_simple_search.java
@@ -16,11 +16,11 @@ public class bst_simple_search {
 		
 	 // creating the refrence of the of node 
 		
-		BinnarySearchTreeNode<Integer>rootnode = allfunction.takeinput(true, 0, false) ; 
+	//	BinnarySearchTreeNode<Integer>rootnode = allfunction.takeinput(true, 0, false) ; 
 		
 		// now again calling for the printing 
 		
-		allfunction.Bst_pritnt(rootnode);
+	//	allfunction.Bst_pritnt(rootnode);
 		
 		// calling the function for the level wise input 
 		
@@ -30,7 +30,8 @@ public class bst_simple_search {
 		
 		// taking the function of the level wise input 
 		BinnarySearchTreeNode<Integer>input = test.takeinput_levelwise() ; 
-		//
+		// creating the print function for the using the different object 
+		allfunction.Bst_pritnt(input);
 		
 
 	}
