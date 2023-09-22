84d99f69eba13f9fcf9f4cf6fba380c9f48aa02d
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-09-22 23:49:29 +0530

    updata

--- BST/BST_CHECK_BINARY_TREE/BinnaryTreeNode.java
+++ BST/BST_CHECK_BINARY_TREE/BinnaryTreeNode.java
@@ -5,7 +5,13 @@ public class BinnaryTreeNode<T> { // creating the genecric
 	// CREATING THE data type t for the data 
 	T data ; 
 	// creating the node 
+	public BinnaryTreeNode<T>leftnode ; 
+	public BinnaryTreeNode<T>rightnode ; 
 	
+	// creating the function for the set the deta 
+	public BinnaryTreeNode(T data ) {
+		this.data = data ; 
+	}
 	
 	
 }
