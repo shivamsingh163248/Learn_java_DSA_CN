9d99092eb7cc36059db8119a4bf94e546d2793cb
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-09-24 23:40:01 +0530

    changed

--- BST/BST_CHECK_BINARY_TREE/input_print_checkbst_function.java
+++ BST/BST_CHECK_BINARY_TREE/input_print_checkbst_function.java
@@ -1,5 +1,21 @@
 package BST_CHECK_BINARY_TREE;
 
+import java.util.Scanner;
+
 public class input_print_checkbst_function {
+	
+	// creating the function for the taking the input and from the user and then 
+	// creating the print function 
+	// creating the check binary tree are the binary search tree or not 
+	
+	public BinnaryTreeNode<Integer> takeinput_levelwise(){
+		
+		// now creating the scanner function for the takeing the input form the user 
+		
+		Scanner input  = new Scanner(System.in) ; 
+		// now creating the display 
+		System.out.println("enter root element ");
+		int rootelement = input.nextInt() ; 
+	}
 
 }
