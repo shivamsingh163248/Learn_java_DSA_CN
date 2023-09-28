9c3bc8997f9f6ed1c96850b43a8c66517989a472
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-09-28 23:14:02 +0530

    update

---
+++ BST/CreatingBstUsingShortedArray/function_BST.java
@@ -0,0 +1,18 @@
+package CreatingBstUsingShortedArray;
+
+import java.util.Scanner;
+
+public class function_BST {
+	//creating the input function level wise input 
+	// creating the function for the printing 
+	
+	public binaryTreeNode<Integer> takeinput_LevelWise(){
+		
+		//creating the scanner object 
+		Scanner input = new Scanner(System.in) ; 
+		// creating the statement for the user dispalay
+		System.out.println("enter root");
+		
+	}
+
+}
