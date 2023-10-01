671d6a8d34a2fbeae81bacf990aa72a515026411
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-10-01 05:58:00 +0530

    changed

---
+++ BST/BSTtoLL/BSTLLfUNCTION.java
@@ -0,0 +1,9 @@
+package BSTtoLL;
+
+import BST_CHECK_BINARY_TREE.input_print_checkbst_function;
+
+public class BSTLLfUNCTION extends input_print_checkbst_function  {
+
+	// NOW CREATING THE FUNCTION FOR THE BST 
+	
+}
---
+++ BST/BSTtoLL/BinaryTreeNode.java
@@ -0,0 +1,16 @@
+package BSTtoLL;
+
+public class BinaryTreeNode <T> { // creating the generic 
+	T data ; 
+	// creating the refreace of the same class of the two object 
+	BinaryTreeNode<T>LeftNode ; 
+	BinaryTreeNode<T>RightNode ; 
+	
+	// now creating the constrocter 
+	
+	public BinaryTreeNode(T data) {
+		// TODO Auto-generated constructor stub
+		this.data = data ;
+	}
+
+}
