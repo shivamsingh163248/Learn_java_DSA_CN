04f1f52166897c5af281224451bf3c56b4036b23
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-10-04 23:56:44 +0530

    commit creating the helper function

--- BST_II/BST_CLASS/BST_CLASS.java
+++ BST_II/BST_CLASS/BST_CLASS.java
@@ -2,6 +2,41 @@ package BST_CLASS;
 
 public class BST_CLASS {
 	
+	// crating the two private member 
+	private BinaryTreeNode<Integer>root ; 
+	private int size; 
+	
+	
 	// NOW creating the function 
+	// creating the helper function of the below function 
+	public boolean isPresentHelper(BinaryTreeNode<Integer>node ,int x  ) {
+		// creating the base condition 
+		if (node == null) {
+			return false ; 
+		}
+		
+		// now creating the condition 
+		
+		if (node.data == x) {
+			return true ; 
+		}
+		
+		// now calling the both side according 
+		if (root.data > x ) {
+			// now caling the left side 
+			return isPresentHelper(node.left, x) ; 
+		}else {
+			return isPresentHelper(node.right, x) ; 
+		}
+		
+		
+	}
+	
+	// now creating the function for the insurt 
+	public boolean isPresent(int x) {
+		
+	}
+	
+
 
 }
