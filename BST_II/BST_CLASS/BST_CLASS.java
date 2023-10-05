78e0f400a73c18a05b82b093e03a7fa07b12ec78
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-10-05 13:09:12 +0530

    changeed

--- BST_II/BST_CLASS/BST_CLASS.java
+++ BST_II/BST_CLASS/BST_CLASS.java
@@ -35,7 +35,61 @@ public class BST_CLASS {
 	// now creating the function for the insurt 
 	public boolean isPresent(int x) {
 		
+		// now calling the helper function 
+		return isPresentHelper(root, x) ; 
+		
 	}
+	
+	// now creating again insurt function  fuction 
+	
+	public void insert(int x ) {
+		
+		// now creating the helper function 
+		
+	} 
+	
+	// now now creating the helper function 
+	public void insertHelper(BinaryTreeNode<Integer>root , int x ) {
+		
+		// now root become head 
+		root = new BinaryTreeNode<Integer>(x) ; 
+	}
+	
+	// now creating the print function 
+	public void print() {
+		
+		
+	}
+	
+	// creating the helper function 
+	private void print(BinaryTreeNode<Integer>root) {
+		
+		// now creating the base condition 
+		if (root == null ) {
+			return ; 
+		}
+		
+		
+		System.out.println("root : "+root.data);
+		
+		// now printing the left and right side 
+		if (root.left != null) {
+			System.out.println("L :"+root.left.data);
+		}
+		// creating the second condition for the right side 
+		if (root.right != null) {
+			System.out.println("R :"+root.right.data);
+		}
+		
+		// now creating for the spcae 
+		System.out.println(); 
+		
+		// now calling the lef side and the right side 
+		print(root.left);
+		print(root.right);
+		
+	}
+	
 	
 
 
