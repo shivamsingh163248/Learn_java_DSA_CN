24fe5514052dd4d8bd3f8c298ff36d0dc68fd9e0
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-10-11 21:28:29 +0530

    changed

---
+++ BST/CHECK_BST/BinnaryTreeNode.java
@@ -0,0 +1,21 @@
+package CHECK_BST;
+
+import PATH_BST.BinaryTreeNode;
+
+public class BinnaryTreeNode <T>{ // creating the generic 
+	
+	T data ; 
+	BinnaryTreeNode<T> left ; 
+	BinaryTreeNode<T> right ; 
+	
+	// now crating the constrocter 
+	
+	public BinnaryTreeNode(T data) {
+		// TODO Auto-generated constructor stub
+		this.data = data ; 
+		
+	}
+
+	
+
+}
