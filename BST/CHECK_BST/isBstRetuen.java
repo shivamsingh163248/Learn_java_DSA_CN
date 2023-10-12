767c4eac981114ed27517ff9106db844029d6bf8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-10-12 10:52:42 +0530

    changed

---
+++ BST/CHECK_BST/isBstRetuen.java
@@ -0,0 +1,24 @@
+package CHECK_BST;
+
+public class isBstRetuen {
+	
+	// crating the thee varaibable min , max  , isbsr 
+	
+	int min ; 
+	int max ; 
+	boolean isBst ; 
+	
+	// now creating the constocter 
+	
+  public isBstRetuen(int min , int max , boolean isBst) {
+	
+	  // now set the value of the all the variable 
+	  this.min = min ; 
+	  this.max = max ; 
+	  this.isBst = isBst ; 
+	  
+}
+  
+  
+
+}
