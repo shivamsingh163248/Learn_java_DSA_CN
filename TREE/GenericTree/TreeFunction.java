bdef1140bc56a3bf32c30f2b98a72e81f0539629
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-10-16 23:52:52 +0530

    changed

--- TREE/GenericTree/TreeFunction.java
+++ TREE/GenericTree/TreeFunction.java
@@ -6,8 +6,14 @@ public class TreeFunction {
 	 // now creating the printing function 
 	public void printTree(TreeNode<Integer>root) {
 		
+		// now writing the base case 
+		if (root == null) {
+			return ; 
+		}
+		
 		// now printing the data 
 		System.out.print(root.data+" ");
+		System.out.println();
 		// now creating the loop 
 		for (int i = 0; i < root.childeren.size(); i++) {
 			// now creating the refrence of the tree 
