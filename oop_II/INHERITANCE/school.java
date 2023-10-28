05b5981ad2d9c7f554d281476e53d8c08b28f583
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-10-28 19:57:22 +0530

    changed

---
+++ oop_II/INHERITANCE/school.java
@@ -0,0 +1,15 @@
+package INHERITANCE;
+
+public class school {
+	
+	// creating the the variable 
+	String department ; 
+	String course ; 
+	
+	
+	// creating the method of the printing 
+	public void print() {
+		System.out.println("this is the school");
+	}
+
+}
