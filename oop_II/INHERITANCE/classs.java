7e3e1f67232bf51613f0144b328ead2b571d1f8b
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-10-28 20:18:26 +0530

    changed

--- oop_II/INHERITANCE/classs.java
+++ oop_II/INHERITANCE/classs.java
@@ -8,8 +8,27 @@ public class classs  extends school{
 	int Roll_no ; 
 	
 	// now creating the print function 
+	
+	// now creating the constorcter 
+
+	
+	
+	
+	
 	public void print() {
 		System.out.println("this is the class ");
 	}
+
+	public classs(String department, String course, String name, int roll_no) {
+		super(department, course);
+		this.name = name;
+		Roll_no = roll_no;
+	}
+
+
+//	public classs() {
+//		super();
+//		System.out.println("this is the super class ");
+//	}
 	
 }
--- oop_II/INHERITANCE/school.java
+++ oop_II/INHERITANCE/school.java
@@ -8,6 +8,29 @@ public class school {
 	
 	
 	// creating the method of the printing 
+	
+	
+	// creating the default counstrocter 
+//	public school() {
+//		System.out.println("this is the default counstoctur ");
+//		System.err.println("my name is the shivam singh ");
+//	}
+//	
+	
+	
+	
+	public school(String department, String course) {
+		
+		this.department = department;
+		this.course = course;
+	}
+
+
+
+
+
+
+
 	public void print() {
 		System.out.println("this is the school");
 	}
