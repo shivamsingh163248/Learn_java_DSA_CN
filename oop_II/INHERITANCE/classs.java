25b2bc0ffe5d190d91746847c3f3539f419ad80a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-10-28 20:02:06 +0530

    changed

---
+++ oop_II/INHERITANCE/classs.java
@@ -0,0 +1,15 @@
+package INHERITANCE;
+
+public class classs  extends school{
+	
+   
+	// now creating the class ]
+	String name ; 
+	int Roll_no ; 
+	
+	// now creating the print function 
+	public void print() {
+		System.out.println("this is the school class ");
+	}
+	
+}
