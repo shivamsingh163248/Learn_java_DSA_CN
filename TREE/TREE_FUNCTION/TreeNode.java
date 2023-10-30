8f6cad49f5255a0cd412c98ecbeb2c41fff72f2a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-10-30 23:16:38 +0530

    changed

---
+++ TREE/TREE_FUNCTION/TreeNode.java
@@ -0,0 +1,15 @@
+package TREE_FUNCTION;
+
+import java.util.ArrayList;
+
+public class TreeNode <T>{
+
+   T Data ;
+   ArrayList<TreeNode<Integer>>children ; 
+   
+   public TreeNode(T data) {
+
+}
+   
+   
+}
