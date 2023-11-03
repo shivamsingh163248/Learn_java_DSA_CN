860624711c7e80efff22fc44f04a0038d939cdc5
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-03 21:35:08 +0530

    hasmap program

--- Hasmap/hasmap_using_bst/HasMapUsingBST_Function.java
+++ Hasmap/hasmap_using_bst/HasMapUsingBST_Function.java
@@ -1,5 +1,32 @@
 package hasmap_using_bst;
 
 public class HasMapUsingBST_Function {
+	
+	// creating the variable 
+	private TreeNode<Integer>root ; 
+	
+	// now creating the constructor of the hasmap using function 
+	
+	public HasMapUsingBST_Function() {
+		// now creating the default value of the treenode is the null 
+		// when creating the object of this class 
+	    root = null ; 
+	}
+	
+	// now creating the insert function 
+	public void put(int key , int value) {
+		
+		// calling the put function 
+		root = insert(root , key , value) ; 
+	}
+
+	private TreeNode<Integer> insert(TreeNode<Integer> root2, int key, int value) {
+		// TODO Auto-generated method stub
+		return null;
+	} 
+	
+	
+	
+	
 
 }
