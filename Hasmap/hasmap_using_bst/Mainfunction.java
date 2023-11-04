660b73692d961cf05a7c121af4f62559c133317e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-04 22:17:25 +0530

    updata

---
+++ Hasmap/hasmap_using_bst/Mainfunction.java
@@ -0,0 +1,34 @@
+package hasmap_using_bst;
+
+public class Mainfunction {
+
+	public static void main(String[] args) {
+		
+		
+		// now creating the main function 
+		HasMapUsingBST_Function function = new HasMapUsingBST_Function() ; 
+		
+		// now creating the add 
+		function.put(1, 234);
+		function.put(2, 32);
+		function.put(3, 734);
+		function.put(4, 325);
+		function.put(5, 24);
+		function.put(6, 302);
+		
+		// now printing the data 
+		System.out.println(function.get(4));
+		System.out.println(function.get(1));
+		System.out.println(function.get(2));
+		System.out.println(function.get(3));
+		System.out.println(function.get(5));
+		System.out.println(function.get(6));
+		
+		
+		// now remove the data 
+		
+		function.put(6, 34);
+		
+	}
+	
+}
