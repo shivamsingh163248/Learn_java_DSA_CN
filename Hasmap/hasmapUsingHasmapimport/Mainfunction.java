f5c1d220d216cded8c931715c11cb86ac339ed57
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-05 10:06:31 +0530

    changed

---
+++ Hasmap/hasmapUsingHasmapimport/Mainfunction.java
@@ -0,0 +1,27 @@
+package hasmapUsingHasmapimport;
+
+import java.util.HashMap;
+
+public class Mainfunction {
+	
+	// CREATING the main class 
+	public static void main(String[] args) {
+		
+		
+		// now creating  the object of the has map 
+		HashMap<Integer, String>student = new HashMap<>() ; 
+		
+		// now add data in the hash map using the opeartion 
+		// using the put operation we can add the data 
+		//@put
+		
+		student.put(2, "my name is the shivam ") ; 
+		student.put(8, "this is the march birthday ");
+		
+		
+		// now is the key are the contain the print the some thing 
+		
+		
+	}
+
+}
