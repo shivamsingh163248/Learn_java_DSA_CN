f285964d94103c7c003abac55237bccfc22837ad
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-06 18:52:30 +0530

    update value

--- Hasmap/hasmapUsingHasmapimport/Mainfunction.java
+++ Hasmap/hasmapUsingHasmapimport/Mainfunction.java
@@ -1,6 +1,7 @@
 package hasmapUsingHasmapimport;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class Mainfunction {
 	
@@ -28,7 +29,44 @@ public class Mainfunction {
 		// now using the remove function for the rtemove any key 
 		student.remove(2) ; 
 		// now we are the testing the 
+		System.out.println(student.get(2));
 		
+		// now creating the contain function 
+		if (student.containsKey(8)) {
+			System.out.println("this valur are the contain ");
+			
+		}
+		
+		// now chacking the if the value are the not conatian then printing the else part 
+		
+		if (student.containsKey(6)) {
+			
+			System.out.println("this is the key contain part");
+			
+		} else {
+            // now else part the using the value of the error
+             System.err.println(" hear the value are the not contain ");
+		}
+		
+		
+		student.put(4, "mahesh") ; 
+		student.put(5, "suresh") ;
+		student.put(6, "dhirendra") ;
+		student.put(7, "gaurav") ; 
+		
+		// now finding the size 
+		System.out.println(student.size());
+		
+		// now creating the set function for the print all the key 
+		// set function are using new keywording 
+		Set<Integer>keyvalue = student.keySet() ; 
+		
+		// now printing the using the for each loop 
+		for (Integer i : keyvalue) {
+			System.out.println(student.get(i));
+		}
+		
+	
 		
 	}
 
