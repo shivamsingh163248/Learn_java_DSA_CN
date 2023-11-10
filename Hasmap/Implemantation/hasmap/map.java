f8973bfac4444b0bbaba250e7082c66c37b4bc30
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-11 04:29:10 +0530

    creating the get function

--- Hasmap/Implemantation/hasmap/map.java
+++ Hasmap/Implemantation/hasmap/map.java
@@ -74,5 +74,35 @@ public class map<T ,V>{
 		
 	
 	}
+	
+	// creating the function of the size 
+	public int size() {
+		return count ; 
+	}
 	 
+	
+	// now creating the function of the get value 
+	public V getValue(T key) {
+		// fist finding the value if the bucket index 
+		int bucketindex = getbucketIndex(key);
+		// now finding the head 
+		mapnode<T, V>head = bucket.get(bucketindex) ; 
+		// now finding it is the null or not if not null then the trevencing the value 
+		// if null then the return the value 
+		while (head != null) {
+			
+			// now creating the condition if the key are the same 
+			if (key.equals(head.key)) {
+				// now the value return the value 
+				return head.value ; 
+			}
+			// if can not find the key 
+			head = head.next ; 
+			
+		}
+		return null ; 
+	}
+	
+	// now creating the function of the size 
+	
 }
