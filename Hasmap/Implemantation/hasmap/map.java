acf8cfc81f7a17d724bcb77cfb77013c3b355ced
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-11 02:56:07 +0530

    changed

--- Hasmap/Implemantation/hasmap/map.java
+++ Hasmap/Implemantation/hasmap/map.java
@@ -1,6 +1,26 @@
 package Implemantation.hasmap;
 
-public class map {
-	
+import java.util.ArrayList;
 
+public class map<T ,V>{
+	
+  // creating the arry list for the taking the arry 
+	// now creating the arry list 
+	ArrayList<mapnode<T, V> >bucket ; 
+	// creating the count 
+	 int count ; 
+	 int numbacket ; 
+	 
+	 // now creating the constrocter 
+	 public map() {
+		// creating the new 
+		bucket = new ArrayList<>() ; 
+		// now creating the the size of the bucket 
+		numbacket = 20 ; 
+		// creating the for loop inisalization of the all is the null 
+		for (int i = 0; i < numbacket; i++) {
+			// now inisilation the null all the bucket 
+			bucket.add(null) ; 
+		}
+	}
 }
