2768e2740833f22809f8e8ac7ed628de901ae77c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-11 03:51:06 +0530

    update the insurt function

--- Hasmap/Implemantation/hasmap/map.java
+++ Hasmap/Implemantation/hasmap/map.java
@@ -30,6 +30,35 @@ public class map<T ,V>{
 		 
 		 // fist getting the bucket index 
 		 int bucketIndex = getbucketIndex(key) ;
+		 // after the finding the index check it is or null or not 
+		 // finding the head node in the bucket 
+		 mapnode<T, V>head = bucket.get(bucketIndex) ; 
+		 // now creating the loop if the not null head then update the value in the hasnode 
+		 while (head != null) {
+			// now finding the proper key for the update the value 
+			 if (key.equals(head.key)) {
+				// now updating the value 
+				 head.value = value ;
+				 
+				 // after the update new we are the return 
+				 
+				 return  ;
+			}
+			 // for the continue loop we are the using the head next in the next loop 
+			 head = head.next ; 
+		}
+		 
+		 
+		 
+		 head = bucket.get(bucketIndex) ; 
+		 // if the value is null then updating the vlaue 
+		 
+		 mapnode<T, V>newNode = new mapnode<>(key, value) ; 
+		 newNode.next = head ; 
+		 // for the making the heading using the bucket set value 
+		 bucket.set(bucketIndex, newNode);
+		 // now count for the finding the number of element in the link list 
+		 count++ ; 
 		 
 		 
 	 }
