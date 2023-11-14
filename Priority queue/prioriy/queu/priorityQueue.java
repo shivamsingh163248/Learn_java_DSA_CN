a1dd2bd5bc8bb4551b4818f5437a9ec10f10f1ab
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-14 16:03:04 +0530

    update priority queue function

--- Priority queue/prioriy/queu/priorityQueue.java
+++ Priority queue/prioriy/queu/priorityQueue.java
@@ -1,5 +1,51 @@
 package prioriy.queu;
 
-public class priorityQueue {
+import java.util.ArrayList;
 
+public class priorityQueue <T> {
+	
+  //CREATING THE array list 
+	ArrayList<Element<T>>heap ; 
+	
+	// creating the constrocter
+	
+	public priorityQueue() {
+	
+		heap = new ArrayList<>() ; 
+	}
+	
+	// creatingthe function insurt , get min remove min , and size , isempty 
+	
+	// fist creating the function of the insert 
+	public void insert(T value ,int priority ) {
+		
+		
+	}
+	// creating the get min function
+	public T getmin() {
+		// 
+	}
+	
+	public T removemin() {
+		
+	}
+	
+	// creating the function is empty
+	public boolean isEmpty() {
+		
+		// when the size of the arrylist is the zero then ismepty 
+		if (heap.size()>0) {
+			return false ; 
+		}else {
+			return true ; 
+		}
+		
+	}
+	
+	// creating the function of the size 
+	public int size() {
+		// return the size of the array list 
+		return heap.size();
+	}
+	
 }
