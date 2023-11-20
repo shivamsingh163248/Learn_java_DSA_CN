52c26d58c269209e5e3baf3735d73981460efe1a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-20 21:09:28 +0530

    update function

--- PriorityQueue II/inbuldPQ/inbuld_PQ.java
+++ PriorityQueue II/inbuldPQ/inbuld_PQ.java
@@ -1,6 +1,10 @@
 package inbuldPQ;
 
-import prioriy.queu.priorityQueue;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+
 
 public class inbuld_PQ {
 	
@@ -9,10 +13,32 @@ public class inbuld_PQ {
 		
 		// NOW creating the inbuilt priority queue that using the peek function and poll function 
 		//and the using much more function 
-		priorityQueue<Integer>shorting = new priorityQueue<>() ; 
-		
+		PriorityQueue<Integer>shorting = new PriorityQueue<>();		
 		// taling the input from the user in the form of the arry 
+		Scanner input = new Scanner(System.in) ; 
+		System.out.println("enter the size of the arry ");
+		// now creating the for loop 
+		int size = input.nextInt();
+		int[]arr = new int[size] ; 
+		// now creating the for loop 
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print("enter: "+i);
+			arr[i] = input.nextInt() ; 
+			
+		}
 		
+		// now adding this array in priority quque 
+		// creating the loop 
+		
+		for (int i : arr) {
+			shorting.add(i) ; 
+		}
+		
+		// now printing the peek and poll of the element 
+		System.out.println(shorting.peek());
+		System.out.println(shorting.poll());
+		System.out.println(shorting.peek());
+				
 		
 	}
 
