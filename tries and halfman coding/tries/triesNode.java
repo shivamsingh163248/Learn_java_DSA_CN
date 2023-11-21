f28ecc91c565481e4eb8740b0b07d55df1e00c94
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-21 14:19:26 +0530

    learning the tries

---
+++ tries and halfman coding/tries/triesNode.java
@@ -0,0 +1,17 @@
+package tries;
+
+public class triesNode {
+	
+	// creating the node for the tries using the concept of the tree 
+	char data ; 
+	boolean isTerminal ; 
+	// now creating the array of this class 
+	triesNode childrean[] ;
+	
+	// now creating the constructor for the adding the data and the insilizer
+	public triesNode(char data ) {
+		// now set the data 
+		
+	}
+
+}
