aa93936efa64025c58bba9f090e39a99e26b79d0
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-21 20:21:29 +0530

    changed

--- tries and halfman coding/tries/triesImplimentFunction.java
+++ tries and halfman coding/tries/triesImplimentFunction.java
@@ -17,8 +17,43 @@ public class triesImplimentFunction {
 	
 	public void add (String world) {
 		
+		// creating the helper function for the insurt the world 
+		addhepler(root , world) ; 
+		
 	}
 	
+	private void addhepler(triesNode root, String world) {
+		
+		// creating the condition for the world is null or not 
+		if (world.length() == 0) {
+			// is means not preset 
+			// now terminal is true 
+			root.isTerminal = true ; 
+			return ;
+		}
+		
+		// find the child index 
+		int childindex = world.charAt(0)- 'A' ; 
+		// after the finding the child index insurint the element 
+		// if element are abalbe 
+		
+		triesNode child = root.childrean[childindex] ; 
+		
+		// if the root is the null 
+		if (child == null) {
+			// now insurting the value in the 
+			child = new triesNode(world.charAt(0)) ; 
+			root.childrean[childindex] = child ; 
+			// hear is the connection 
+		}
+		
+		
+		// now again calling 
+		addhepler(child , world.substring(1));
+		
+		
+	}
+
 	// creting the remove function 
 	public boolean search (String world) {
 		
