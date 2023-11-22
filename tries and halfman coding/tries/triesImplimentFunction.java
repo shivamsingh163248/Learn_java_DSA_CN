6dcae54d919de384de7f486fe33faf7b39fd0292
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-22 09:33:47 +0530

    changed

--- tries and halfman coding/tries/triesImplimentFunction.java
+++ tries and halfman coding/tries/triesImplimentFunction.java
@@ -57,8 +57,38 @@ public class triesImplimentFunction {
 	// creting the remove function 
 	public boolean search (String world) {
 		
+		// creating the base condition 
+		// creating the helper function 
+		return searchHelpar(root , world) ; 
+		
 	}
 	
+	private boolean searchHelpar(triesNode root, String world) {
+	
+		// creating the base condtition 
+		if (world.length() == 0) {
+			return root.isTerminal ; 
+		}
+		
+		
+		// now checking the function 
+		// frist finding the index of the char so finding char index 
+		int childindex = world.charAt(0) -'A' ; 
+		// storing the childindex address in the another reference 
+		
+		triesNode child  = root.childrean[childindex] ; 
+		
+		// if child is the find the null then return false 
+		if (child == null) {
+			return false ; 
+		}
+		
+		// now calling for the another child 
+		return searchHelpar(child, world.substring(1)) ; 
+		
+		
+	}
+
 	// creating the remove function 
 	public void remove(String world) {
 		
