6185a70e2633594a591cc94886dd01d9d9b01e44
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-24 11:22:44 +0530

    creating the constocter

--- tries and halfman coding/countworldfunctionTries/triesfunction.java
+++ tries and halfman coding/countworldfunctionTries/triesfunction.java
@@ -1,5 +1,22 @@
 package countworldfunctionTries;
 
 public class triesfunction {
+	
+	// creating the root 
+	private TriesNode root ; 
+	
+	// now creating the constructor 
+	
+	public triesfunction() {
+		
+		// now set the null char in java '\0' ; 
+		 root = new TriesNode('\0') ; 
+	}
+	
+	// now creating the add function 
+	// creating the remove function 
+	// creating the search function 
+	
+	
 
 }
