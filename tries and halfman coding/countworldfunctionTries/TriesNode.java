c9ba02e00b909c071578206b39f8e1fdb3412662
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-24 09:39:39 +0530

    creating the tries NODE

--- tries and halfman coding/countworldfunctionTries/TriesNode.java
+++ tries and halfman coding/countworldfunctionTries/TriesNode.java
@@ -1,5 +1,29 @@
 package countworldfunctionTries;
 
-public class TriesNode {
 
+import tries.triesNode;
+
+public class TriesNode {
+	
+	 // fist creatin the data 
+	char data ; 
+	boolean isTerminal ; 
+	// class type creating the childrean 
+	public TriesNode[]children  ; 
+	// creating the count function 
+	int count ; 
+	
+	// now creating the constocter 
+	 public TriesNode(char data) {
+		this.data = data ; 
+		isTerminal = false ;
+		children = new TriesNode[26] ; 
+		count = 0 ; 
+		
+	}
+	
+	
+	
+	
+	
 }
