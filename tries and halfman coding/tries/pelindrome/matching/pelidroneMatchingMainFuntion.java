6bf29a084f1f919381c9dc04cc50db457e5a8516
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-11-30 01:37:22 +0530

    update

--- tries and halfman coding/tries/pelindrome/matching/pelidroneMatchingMainFuntion.java
+++ tries and halfman coding/tries/pelindrome/matching/pelidroneMatchingMainFuntion.java
@@ -1,6 +1,7 @@
 package tries.pelindrome.matching;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class pelidroneMatchingMainFuntion {
 	
@@ -8,7 +9,20 @@ public class pelidroneMatchingMainFuntion {
 	public static ArrayList<String> takeinput(){
 		
 		// creating the object of the array list 
+		ArrayList<String>collectionWorld = new ArrayList<>();
+		// creating the loop for the adding element 
+		Scanner input = new Scanner(System.in);
 		
+		System.out.println("enter the size of n ");
+		int size = input.nextInt();
+		// now creating the loop 
+		for (int i = 0; i < size; i++) {
+			System.out.print(i+" ");
+			String world = input.next();
+		    collectionWorld.add(world);	
+		}
+		
+		return collectionWorld ; 
 	}
 
 	public static void main(String[] args) {
