7420183dde82dc2bdf48e54a70bf8ae38f23587f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-12-04 02:34:22 +0530

    update

--- recursion - 3/recursion/subsquence/string/subsquenceString.java
+++ recursion - 3/recursion/subsquence/string/subsquenceString.java
@@ -1,10 +1,12 @@
 package recursion.subsquence.string;
 
+import java.util.Scanner;
+
 public class subsquenceString {
 	
 	// creating the method for the finding subsequence 
 	public void subsequence(String world , String stringSoFor) {
-		
+		System.out.println("world : - "+world+" , "+"substring : - "+stringSoFor);
 		// now creating the base condition 
 		if (world.length() == 0 ) {
 			System.out.println(stringSoFor);
@@ -13,6 +15,10 @@ public class subsquenceString {
 		
 		
 		String chars = world.substring(1) ; 
+		// now creating the 
+		 subsequence(chars, stringSoFor);
+		
+		 subsequence(chars, stringSoFor+world.charAt(0));
 		
 		 
 		
---
+++ recursion - 3/recursion/subsquence/string/subsquenceStringMain.java
@@ -0,0 +1,13 @@
+package recursion.subsquence.string;
+
+public class subsquenceStringMain {
+
+	public static void main(String[] args) {
+	
+		// creating the object 
+		subsquenceString result = new subsquenceString();
+		result.subsequence("abc", "");
+
+	}
+
+}
