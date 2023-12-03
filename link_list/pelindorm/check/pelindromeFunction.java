bbfd7f67d68bfd6e80265171e29722536e667b6c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-12-04 05:10:39 +0530

    update

--- link_list/pelindorm/check/pelindromeFunction.java
+++ link_list/pelindorm/check/pelindromeFunction.java
@@ -1,9 +1,46 @@
 package pelindorm.check;
 
+import java.util.Scanner;
+
 public class pelindromeFunction {
   
 	 // creating the fuction for the taking the input 
-	
+	public LinklistNode<Integer> takinginput(){
+		
+		// now inisilzed the 
+		LinklistNode<Integer>head  = null , tail = null ;  
+		// creating the loop for the intput 
+		Scanner input = new Scanner(System.in);
+		
+		// creating the condition -1  
+		// creating the loop 
+		int num = input.nextInt();
+		
+		
+		while (num != -1) {
+		
+			// now creating the refrence of the linklist
+			
+				LinklistNode<Integer>node = new LinklistNode<Integer>(num) ; 
+				if (head == null) {
+					head = node ; 
+					tail = node; 
+				}else {
+					tail.next = node ; 
+					tail = node ; 
+					 
+				}
+				
+				num = input.nextInt();
+			
+			
+			
+			
+			
+		}
+		return head ; 
+		
+	}
 	
 	
 }
