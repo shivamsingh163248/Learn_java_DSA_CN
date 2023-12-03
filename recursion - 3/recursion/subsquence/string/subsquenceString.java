10cde4fa887fe6b890b1a67d751db2510a8d6fb4
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-12-03 14:38:33 +0530

    update

---
+++ recursion - 3/recursion/subsquence/string/subsquenceString.java
@@ -0,0 +1,22 @@
+package recursion.subsquence.string;
+
+public class subsquenceString {
+	
+	// creating the method for the finding subsequence 
+	public void subsequence(String world , String stringSoFor) {
+		
+		// now creating the base condition 
+		if (world.length() == 0 ) {
+			System.out.println(stringSoFor);
+			return ; 
+		}
+		
+		
+		String chars = world.substring(1) ; 
+		
+		 
+		
+		
+	}
+
+}
