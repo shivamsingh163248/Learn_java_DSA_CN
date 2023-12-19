30c4cf71c060c38a7577bf7b114a0137441cbd95
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-12-19 21:44:48 +0530

    creating the substring function

--- recursion - 3/recoresen/subset/array/number_subset_array.java
+++ recursion - 3/recoresen/subset/array/number_subset_array.java
@@ -15,9 +15,23 @@ public class number_subset_array {
 		int[]arr = new int[n] ; 
 		for (int i = 0; i < arr.length; i++) {
 			// now insurt array 
+			System.out.println(i);
+			arr[i]= input.nextInt();
 			
-			arr[i] = input.nextInt();
 		}
+		return arr ;
+	}
+	
+	
+	// creating the function for the finding the all the subset 
+	public int[][]subsetHelper(int[]arr , int startIndex){
+		
+		// creating the base function 
+		if (arr.length == startIndex) {
+			int[][] output = new int[1][0];
+			return output ; 
+		}
+		
 	}
 	
 	
