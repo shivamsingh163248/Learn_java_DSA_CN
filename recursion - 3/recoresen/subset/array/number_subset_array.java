d86c07292a9f5f582c63ee6a704208fbd2d31e2d
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-12-19 19:25:07 +0530

    updte

---
+++ recursion - 3/recoresen/subset/array/number_subset_array.java
@@ -0,0 +1,17 @@
+package recoresen.subset.array;
+
+import java.util.Scanner;
+
+public class number_subset_array {
+
+	 // creating the taking the input  and output given in the array formet 
+	public int[] takeinput() {
+		// now taking the user input 
+		System.out.println("enter the number");
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		
+	}
+	
+	
+}
