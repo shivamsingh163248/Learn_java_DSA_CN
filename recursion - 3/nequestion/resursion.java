550b163b0e2b45d14a0771360c036b3351f8f33d
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-12-24 19:45:38 +0530

    update

--- recursion - 3/nequestion/resursion.java
+++ recursion - 3/nequestion/resursion.java
@@ -5,15 +5,23 @@ import java.util.Scanner;
 public class resursion {
 
 	 // creating the new function for the solving the problem 
-	public static taking_input() {
+	public int[]  taking_input() {
 		// now this the paremetraic function for the taking the input 
 	Scanner input = new Scanner(System.in);
 	System.out.println("eneter the number");
 	
+	// now insizalization the method 
+	
 		int n =  input.nextInt();
-		
+		int[]arr = new int[n];
 		// now creating the loop for the solving the problem 
-		for (int i = 0; i < array.length; i++) {
+		// now creating the loop 
+		for (int i = 0; i < arr.length; i++) {
+			
+			// each loop have the each number
+			// now creating the index of the nunber
+			System.out.println("index"+i);
+			arr[i] = input.nextInt();
 			
 		}
 		
