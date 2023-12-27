ac418ea60d0f550b7c17064ebdf2e10e39c4b989
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-12-27 22:55:50 +0530

    update

--- recursion - 3/All/Unique/subset/AllUniqueSubset.java
+++ recursion - 3/All/Unique/subset/AllUniqueSubset.java
@@ -1,12 +1,22 @@
 package All.Unique.subset;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AllUniqueSubset {
 	
 	// print all unique subset
 	// creating the function of the getsubset array 
 	public ArrayList<ArrayList<Integer>> getArray(int[]input){
+		Arrays.sort(input);
+		// creating the object of the subset and current subset of the arraylist 
+		ArrayList<ArrayList<Integer>>subset = new ArrayList<>();
+		ArrayList<Integer>currentSubset = new ArrayList<>() ; 
+		
+		// now calling the function of generate subset 
+		generateSubsets(input, 0, currentSubset, subset);
+		return subset ; 
+		
 		
 	}
 	
@@ -16,9 +26,31 @@ public class AllUniqueSubset {
 
 		subset.add(new ArrayList<>(currentSubset));
 		
-		// now creating the loop 
+		// now creating the for loop 
+		for (int i = 0; i < input.length; i++) {
+			
+			if ( i > index && input[i] == input[i-1]) {
+				continue ; 
+			}
+			
+			currentSubset.add(input[i]);
+			
+			// now calling the same function for the adding the list 
+			generateSubsets(input, i+1, currentSubset, subset);
+			currentSubset.remove(currentSubset.size()-1) ; 
+			
+			
+		}
+		
+		
+		
 		
 	}
+	
+	
+	
+	// creating the print function for the sunset 
+	
 	
 	
 	}
