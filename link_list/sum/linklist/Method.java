3b6139946527e6a71589d752dbc2d277b4ff1cb8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-01-01 22:18:23 +0530

    update

--- link_list/sum/linklist/Method.java
+++ link_list/sum/linklist/Method.java
@@ -1,6 +1,73 @@
 package sum.linklist;
 
+import java.util.Scanner;
+
 public class Method {
+	
+	
+	// printing the linklist 
+	public void print(ListNode node) {
+		
+		// now printing the node 
+		
+			System.out.println(node.val+" ");
+			node = node.next ; 
+			System.out.println(node.val+" ");
+			node = node.next ; 
+			System.out.println(node.val+" ");
+			node = node.next ; 
+			System.out.println(node.val+" ");
+		
+		
+	}
+	
+	public ListNode revesing(ListNode node) {
+		
+		// creating the object of the list node 
+		// creating the using recussrison 
+		
+		if (node == null) {
+			return null ; 
+		}
+		
+		
+		ListNode output =   revesing(node.next) ; 
+			 // now creating the algorithum 
+		// creating the new 
+		ListNode samllout = new ListNode(node.val) ; 
+	      samllout.next = output ; 
+		return samllout ; 	
+		
+		
+	}
+	
+	// now creating the linklist 
+	
+	public ListNode insert() {
+		
+	 // taking the user input 
+		System.out.println("enter the number");
+		Scanner input = new Scanner(System.in) ; 
+		int n = input.nextInt();
+		// creating the object of the node 
+		// creating the condition if -1 then output 
+		ListNode head = null ; 
+		ListNode tail = null ; 
+		
+		
+		while (n != -1) {
+			
+			head = new ListNode(n) ; 
+			System.out.println("enter the number");
+			 n = input.nextInt();
+			 head.next = tail ; 
+			 tail = head ; 
+			
+		}
+		return head ; 
+		
+	}
+	
 	
 	  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 	        
@@ -10,7 +77,7 @@ public class Method {
 
 	        while(l1 != null && l2 != null){
 	         
-
+                  
 	             
 	         l1 = l1.next ;
 	         l2 = l2.next ; 
---
+++ link_list/sum/linklist/sum.java
@@ -0,0 +1,15 @@
+package sum.linklist;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Method check = new Method() ; 
+		
+		ListNode insurt  = check.insert() ; 
+		check.print(insurt);
+		
+	}
+
+}
