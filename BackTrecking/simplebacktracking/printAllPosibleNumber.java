f4e3420b22c72d2baff0e26ab7e56c1d7da66958
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-01-06 20:13:12 +0530

    update

--- BackTrecking/simplebacktracking/printAllPosibleNumber.java
+++ BackTrecking/simplebacktracking/printAllPosibleNumber.java
@@ -5,16 +5,39 @@ public class printAllPosibleNumber {
 	// creating the method 
 	
 	// creating the print method 
-	public void print(int[]arr) {
+	public static  void print(int[]arr) {
 		// creating the for each loop 
 		for (int i : arr) {
 			System.out.print(i+" ");
 		}
+		System.out.println();
 	}
 	
 	
-	public void printAllPosiblenumber(int[]arr , int n ) {
+	public static  void printAllPosiblenumber(int[]arr , int n  , int i  ) {
 		
+		if (n == i) {
+			print(arr);
+			return  ; 
+		}
+		
+		// now calling the same function 
+		arr[i] = 1 ; 
+		printAllPosiblenumber(arr, n, i+1);
+		
+		arr[i] = 2 ; 
+		printAllPosiblenumber(arr, n, i+1);
+		
+		return ; 
+		
+		
+	}
+	
+	// creating the main function 
+	public static void main(String[] args) {
+		
+		int[]arr = {1,2,1} ; 
+		printAllPosiblenumber(arr, arr.length, 0);
 	}
 
 }
