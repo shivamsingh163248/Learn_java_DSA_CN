94aba489fabb8d8072c8e3dcadcb7a14da8dfcc2
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-02-07 16:07:18 +0530

    learaning the leet conde problem

---
+++ leetCodeProblem/Easy/FirstUniqueCharacterinaString387.java
@@ -0,0 +1,29 @@
+package Easy;
+
+import java.util.Scanner;
+
+public class FirstUniqueCharacterinaString387 {
+	
+	
+	// creating the input function 
+	
+	public static String takieinput() {
+		
+		// creating the scanner object 
+		Scanner input = new  Scanner(System.in) ; 
+		// calling input from the user 
+		System.out.println("enter the string ");
+		String world = input.next() ; 
+		return world ; 
+	}
+	
+      public int firstUniqChar(String s) {
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
