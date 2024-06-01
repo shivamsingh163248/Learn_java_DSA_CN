85d5411f30eb646bc9130d7cb9bfcdb0be0c8a09
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-01 14:01:18 +0530

    learning the rivision the basi of java

---
+++ data-type-java/oopconcept/employ.java
@@ -0,0 +1,26 @@
+package oopconcept;
+
+public class employ {
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getDate() {
+		return date;
+	}
+	public void setDate(Integer date) {
+		this.date = date;
+	}
+	// creating the access modifier 
+	private  String name  ;
+	private  Integer date ; 
+	
+	// creating the setter and getter
+	
+	
+	
+
+}
