b7f83153d950c925f6bb5d984f91e319c1697987
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-04 17:51:59 +0530

    update

---
+++ data-type-java/conversion/LearingTestingChar.java
@@ -0,0 +1,16 @@
+package conversion;
+
+public class LearingTestingChar {
+	
+	// creating the the test 
+	public static void main(String[] args) {
+		
+		
+	 
+		byte a = 127 ; 
+		System.out.println((byte)(a+1));
+	}
+	
+	
+
+}
