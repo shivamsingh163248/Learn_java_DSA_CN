3cf0009cb45c0cdf5dd53e5e0b8f8fc8e4be1a6a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-08-17 23:58:13 +0530

    finding the last digit of element

--- codingninja/arrayproblem.java
+++ codingninja/arrayproblem.java
@@ -8,6 +8,11 @@ public class arrayproblem {
 		Scanner input = new Scanner(System.in) ;  
 		// this is the scanner function that are the using for the taking the input from the user 
 		int n = input.nextInt() ; 
+		// creating the form we are the finding the last digit number 
+		for (int i = n; i > 0; i = i/10) {
+			System.out.println(i%10+" "+i);
+		}
+		
 
 	}
 
