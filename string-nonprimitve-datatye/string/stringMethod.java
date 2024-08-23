8ef5fba840c6b094eadb157930de9a71393c4f86
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-08-24 01:03:25 +0530

    updating the value

--- string-nonprimitve-datatye/string/stringMethod.java
+++ string-nonprimitve-datatye/string/stringMethod.java
@@ -1,11 +1,31 @@
 package string;
 
+import java.util.Scanner;
+
 public class stringMethod {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		// now are  are the testing all the string method 
+		// creating the scanner function 
+		Scanner input  = new Scanner(System.in) ; 
+		
+		String value = input.next() ;
+		
+		String v1 = "shivam" ; 
+		String v2 = "singh" ; 
+		
+		System.out.println(v1+v2);
+		
+		v1 += v2 ;
+		
+		// now again printing the value 
+		System.out.println(v1);
+		
+		// again using the concatenation method to connect the string 
+		System.out.println(v1.concat(v2));
+		
 		
 
 	}
