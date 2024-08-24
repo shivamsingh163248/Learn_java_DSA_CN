46f9bb2ad04393129149687bdb9c00cd0460554e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-08-24 15:08:56 +0530

    updating the reversing funcion

--- string-nonprimitve-datatye/string/propertyString.java
+++ string-nonprimitve-datatye/string/propertyString.java
@@ -17,9 +17,27 @@ public class propertyString {
 	// again creating the function for the reversing the  string 
 	// this is the using for the reversing the string 
 	public static String reverseString(String str) {
-		// this are the using the reversing the string 
+		// this are the using the reversing the string
+		String ans = "" ; 
+		// now creating the loop for the adding the char 
+		for (int i = str.length()-1; i >= 0 ; i++) {
+			
+			// using the char method to finding the value that are the required 
+			ans +=  str.charAt(i) ;
+			
+			
+		
+		}
+		
+		// now returning the ans 
+		return ans ; 
+		
 		
 	}
+	
+	// creating the method of the checking the pelindrome
+	// now updating the file 
+	
 	
 	
 	
