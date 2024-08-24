c3445fa634ea59e9cfa1b6163719931de0cf8b03
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-08-24 15:01:01 +0530

    updating the code

---
+++ string-nonprimitve-datatye/string/propertyString.java
@@ -0,0 +1,33 @@
+package string;
+
+import java.util.Scanner;
+
+public class propertyString {
+	
+	// creating the method of the scanning of the striing 
+	public static String userInput() {
+		
+		// calling the scanner librebary 
+		Scanner input  = new Scanner(System.in) ; 
+		
+		String userInputString = input.next() ; 
+		return userInputString ; 
+	}
+	
+	// again creating the function for the reversing the  string 
+	// this is the using for the reversing the string 
+	public static String reverseString(String str) {
+		// this are the using the reversing the string 
+		
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
