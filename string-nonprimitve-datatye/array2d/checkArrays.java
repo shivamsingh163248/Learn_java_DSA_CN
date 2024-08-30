c7aa4464d8b9894f879cc3d4519d2e72aba8214c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-08-30 19:37:04 +0530

    updating the values

--- string-nonprimitve-datatye/array2d/checkArrays.java
+++ string-nonprimitve-datatye/array2d/checkArrays.java
@@ -1,6 +1,30 @@
 package array2d;
 
+import java.util.Scanner;
+
 public class checkArrays {
+	
+	public static int[][] arrysinput(){
+		
+		// creating the 2d arrays and the taking the input in the arrays
+		// creating the scanner function for the size of the arrays 
+		Scanner input = new Scanner(System.in) ; 
+		// calling for the users 
+		System.out.println("enter the dimension of the matrix in the form of n*m");
+		int row = input.nextInt() ; 
+		int column = input.nextInt() ; 
+		int[][]ans = new int[row][column] ; 
+		
+		// now creating the loop for the insurting the values 
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				
+			}
+		}
+		
+		// now returning the ans 
+		return ans  ; 
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
