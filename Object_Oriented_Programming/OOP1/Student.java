624a49830805e2c0ebc002711a1a43dcc4cbd504
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-09-16 14:48:10 +0530

    updating the values

--- Object_Oriented_Programming/OOP1/Student.java
+++ Object_Oriented_Programming/OOP1/Student.java
@@ -1,9 +1,24 @@
 package OOP1;
 
 public class Student {
-
+	
 	String name ; 
 	int roll_no ; 
+
+	private String getName() {
+		return name;
+	}
+	private void setName(String name) {
+		this.name = name;
+	}
+	private int getRoll_no() {
+		return roll_no;
+	}
+	private void setRoll_no(int roll_no) {
+		this.roll_no = roll_no;
+	}
+	
+	
 	
 
 }
