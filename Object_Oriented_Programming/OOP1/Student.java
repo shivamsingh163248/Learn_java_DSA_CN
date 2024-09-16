3f92ee92e0d47616602ccac465b2d065e7e1455c
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-09-16 15:06:34 +0530

    learning the access modifire

--- Object_Oriented_Programming/OOP1/Student.java
+++ Object_Oriented_Programming/OOP1/Student.java
@@ -1,24 +1,27 @@
 package OOP1;
 
 public class Student {
-	
-	String name ; 
-	int roll_no ; 
 
-	private String getName() {
+	protected String getName() {
 		return name;
 	}
-	private void setName(String name) {
+	protected void setName(String name) {
 		this.name = name;
 	}
-	private int getRoll_no() {
+	protected int getRoll_no() {
 		return roll_no;
 	}
 	private void setRoll_no(int roll_no) {
 		this.roll_no = roll_no;
 	}
+	String name ; 
+	int roll_no ; 
 	
 	
+	void shivam() {
+		setName("shivam ");
+		setRoll_no(roll_no);
+	}
 	
 
 }
--- Object_Oriented_Programming/OOP1/school.java
+++ Object_Oriented_Programming/OOP1/school.java
@@ -7,16 +7,19 @@ public class school {
 		// creating the object of school 
 		Student shivam = new Student() ; 
 		
-		// Checking for the printing for the object 
-		System.out.println(shivam);
-		System.out.println(shivam.name);
-        System.out.println(shivam.roll_no);
-        
-        shivam.name = "shivam singh " ; 
-        shivam.roll_no = 242211017 ; 
-        
-        System.out.println(shivam.name);
-        System.out.println(shivam.roll_no);
+//		// Checking for the printing for the object 
+//		System.out.println(shivam);
+//		System.out.println(shivam.name);
+//        System.out.println(shivam.roll_no);
+//        
+//        shivam.name = "shivam singh " ; 
+//        shivam.roll_no = 242211017 ; 
+//        
+//        System.out.println(shivam.name);
+//        System.out.println(shivam.roll_no);
+		
+		// creating the 
+		shivam.getName()
 	}
 
 }
