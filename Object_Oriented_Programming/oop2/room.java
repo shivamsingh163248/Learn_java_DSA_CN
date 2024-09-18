d8c295ce14b8328b352aadb5bebe9226d4c5f4f5
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-09-18 22:45:10 +0530

    updating the values

--- Object_Oriented_Programming/oop2/room.java
+++ Object_Oriented_Programming/oop2/room.java
@@ -2,51 +2,17 @@ package oop2;
 
 public class room {
 	
-	// creating the private member of the variable 
-	String name ; 
-	String address ; 
-	int aadhar_card_nunber ;
-	String hight ; 
-	String father_name ; 
-	String mother_name ; 
-
-	protected String getName() {
-		return name;
+	public static void main(String[] args) {
+		
+		identity anuj = new identity() ; 
+		anuj.setName("anuj");
+		anuj.setAadhar_card_nunber(345445465);
+		anuj.setFather_name("anil");
 	}
-	protected void setName(String name) {
-		this.name = name;
-	}
-	protected String getAddress() {
-		return address;
-	}
-	protected void setAddress(String address) {
-		this.address = address;
-	}
-	protected int getAadhar_card_nunber() {
-		return aadhar_card_nunber;
-	}
-	protected void setAadhar_card_nunber(int aadhar_card_nunber) {
-		this.aadhar_card_nunber = aadhar_card_nunber;
-	}
-	protected String getHight() {
-		return hight;
-	}
-	protected void setHight(String hight) {
-		this.hight = hight;
-	}
-	protected String getFather_name() {
-		return father_name;
-	}
-	protected void setFather_name(String father_name) {
-		this.father_name = father_name;
-	}
-	protected String getMother_name() {
-		return mother_name;
-	}
-	protected void setMother_name(String mother_name) {
-		this.mother_name = mother_name;
-	}
-
+	
+	
+        
+	
 
 	
 	
