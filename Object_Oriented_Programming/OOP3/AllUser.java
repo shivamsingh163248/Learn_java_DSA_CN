a4060b7a8826a10c8c8c38f71fecc39a5d593711
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-09-20 18:10:45 +0530

    updating the values

--- Object_Oriented_Programming/OOP3/AllUser.java
+++ Object_Oriented_Programming/OOP3/AllUser.java
@@ -1,5 +1,20 @@
 package OOP3;
 
+import java.util.Scanner;
+
 public class AllUser {
+	
+	// crating the arrys and the all user 
+	private user all_User[] ; 
+	
+	// creating the function for the storing data 
+	
+    public void Count() {
+		
+    	// creating the scanner function 
+    	Scanner input = new Scanner(System.in) ; 
+
+	}
+	
 
 }
--- Object_Oriented_Programming/OOP3/user.java
+++ Object_Oriented_Programming/OOP3/user.java
@@ -1,5 +1,58 @@
 package OOP3;
 
 public class user {
+	
+	
+  // creating the parameter about the user and then creating type of the constructor
+ private String name ; 
+  private String Father_name ; 
+  private String Mother_name ; 
+  private int Aadhar ; 
+  
+  // creating the default constructor 
+  public user() {
+	
+}
+  public user(String name) {
+	  
+	  this.name  = name ; 
+  }
+  
+  public user(String name , String father_name , String mother_name , int Adhar_nu) {
+	  // using the this key world for the 
+	  this.name = name ; 
+	  this.Father_name = father_name ; 
+	  this.Mother_name = mother_name ; 
+	  this.Aadhar = Adhar_nu ; 
+  }
+  
+  // creating the getter and the setter values that are the using 
+  
+  // creating the getter and setter vaslues 
+  
+  public int getAadhar() {
+	return Aadhar;
+}
+  public String getFather_name() {
+	return Father_name;
+}
+  public String getMother_name() {
+	return Mother_name;
+}
+  public String getName() {
+	return name;
+}
+  public void setAadhar(int aadhar) {
+	Aadhar = aadhar;
+}
+  public void setFather_name(String father_name) {
+	Father_name = father_name;
+}
+  public void setMother_name(String mother_name) {
+	Mother_name = mother_name;
+}
+  public void setName(String name) {
+	this.name = name;
+}
 
 }
