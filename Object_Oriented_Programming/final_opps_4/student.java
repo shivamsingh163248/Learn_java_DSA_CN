4d60109a2bfddd890cb9bdca7acb5a30c8a7c189
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-09-21 01:19:03 +0530

    upating the velus

--- Object_Oriented_Programming/final_opps_4/student.java
+++ Object_Oriented_Programming/final_opps_4/student.java
@@ -3,6 +3,12 @@ package final_opps_4;
 public class student {
 	
 	// creating the some final variable 
+	final int roll_nulber ; 
+	String name  ; 
 	
-
+     // creating the conductor 
+	public student(int value) {
+		roll_nulber = value ; 
+	}
+	
 }
