94735dfddc8d8f3b08daaee58cb3529aa576c588
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-09-20 12:56:49 +0530

    updating and the lerning the counstoctur and the type

--- Object_Oriented_Programming/oop2/identity.java
+++ Object_Oriented_Programming/oop2/identity.java
@@ -16,6 +16,7 @@ public class identity {
 	
 
 
+
 	protected String getName() {
 		return name;
 	}
--- Object_Oriented_Programming/oop2/room.java
+++ Object_Oriented_Programming/oop2/room.java
@@ -10,7 +10,15 @@ public class room {
 		anuj.setFather_name("anil");
 		
 		anuj.i_roll = 45445 ; 
-		anuj.i_hegiht = 56 ; 
+		System.out.println(anuj.i_hegiht);
+		
+		// now printing the values and the object 
+		System.out.println(anuj); 
+		
+		// and the printing the values 
+		
+		
+		
 		
 	}
 	
