8ac7b15b063eace883f5139a011a673aa7ca154a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-09-26 20:26:24 +0530

    updating the values

--- Object_Oriented_Programming/static_final_expalin_use/Student_class.java
+++ Object_Oriented_Programming/static_final_expalin_use/Student_class.java
@@ -1,5 +1,74 @@
 package static_final_expalin_use;
 
+import java.util.ArrayList;
+
 public class Student_class {
+	
+
+	// creating the arrays of the class 
+	// creating the arrays list  list 
+	ArrayList<student> allStudents ; 
+	
+	student[] allStudent ; 
+	// also crating the arrays 
+	
+	// writing the property of the class 
+	private int StudentClass  ; 
+	private String className ; 
+	private static String classTeacher ;
+	
+	
+	public Student_class(student[] allStudent, int studentClass, String className) {
+		super();
+		this.allStudent = allStudent;
+		StudentClass = studentClass;
+		this.className = className;
+	} 
+	
+	
+	
+	// creating the function for the adding the all the student 
+	private student addStudent (int mathMrk, int scineceMark, int englishMark ,String studentName) {
+		return new student(mathMrk, scineceMark, englishMark, studentName) ; 
+	}
+	
+	
+	// now adding the studnet 
+	
+	public void AddStudentDetail () {
+		allStudents.add(addStudent(StudentClass, StudentClass, StudentClass, className)) ; 
+	}
+	
+	
+	
+	
+	
+	
+
+	protected int getStudentClass() {
+		return StudentClass;
+	}
+	protected void setStudentClass(int studentClass) {
+		StudentClass = studentClass;
+	}
+	protected String getClassName() {
+		return className;
+	}
+	protected void setClassName(String className) {
+		this.className = className;
+	}
+	protected static String getClassTeacher() {
+		return classTeacher;
+	}
+	protected static void setClassTeacher(String classTeacher) {
+		Student_class.classTeacher = classTeacher;
+	}
+	
+	
+
+	
+	
+	
+	
 
 }
