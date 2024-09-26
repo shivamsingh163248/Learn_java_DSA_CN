37f481115ecba5645772711f285fb6f30fdeba11
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-09-26 19:11:12 +0530

    updating the values

--- Object_Oriented_Programming/static_final_expalin_use/student.java
+++ Object_Oriented_Programming/static_final_expalin_use/student.java
@@ -3,6 +3,34 @@ package static_final_expalin_use;
 public class student {
 	
 	// writing the student property writing the basic rule of the student 
+	private String studentName ; 
+	private  int studentRollNumber ;
+	private static int toalStudent ; 
+	
+	// creating the result program 
+	private final int mathMrk ; 
+	private final int EnglishMark ; 
+	private final int ScineceMark ;
+	
+	
+	
+	// creating the constructor 
+public student(int mathMrk, int scineceMark, int englishMark ,String studentName ) {
+	
+	toalStudent++ ; 
+	studentRollNumber++ ; 
+	this.mathMrk = mathMrk;
+	this.EnglishMark = englishMark;
+	this.ScineceMark = scineceMark;
+	this.studentName = studentName ; 
+	
+}
+
+
+
+	
+	
+	
 	
 
 }
