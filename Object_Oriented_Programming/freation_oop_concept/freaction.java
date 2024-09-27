f17510832f9433b3ed10f3148ba34aa92b0d139e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-09-27 21:51:23 +0530

    updating the valus and the learning the all the concepts

---
+++ Object_Oriented_Programming/freation_oop_concept/freaction.java
@@ -0,0 +1,48 @@
+package freation_oop_concept;
+
+import shivam.frection;
+
+public class freaction {
+	
+	// creating the variable 
+	int nominator ; 
+	int denominator ; 
+	
+	
+	// creating the add  function 
+	
+	public void  add (freaction num) {
+		
+		// returning the values 
+		
+		
+		
+	}
+	
+	// creating the simply function 
+	public void simpliy(freaction num) {
+		
+		// find the greatest common division 
+		int minvalues = Math.min(nominator, denominator) ; 
+		
+		
+		
+	}
+	public void increment(freaction num) {
+		
+	}
+	
+	public void multi(freaction num) {
+		
+		
+	}
+	
+	public void print() {
+		
+		
+	}
+	
+	
+	
+
+}
