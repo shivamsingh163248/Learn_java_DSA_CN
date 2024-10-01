bb7ea12d462c3f0db7084052cb1c17b300511272
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-10-01 19:43:03 +0530

    updating and learning the dyanmic arrys

--- Object_Oriented_Programming/Dynamic_arrys/ArraysFunction.java
+++ Object_Oriented_Programming/Dynamic_arrys/ArraysFunction.java
@@ -7,5 +7,47 @@ public class ArraysFunction {
 	// creating the arrays 
 	int[]arr ; 
 	// creating the constructor 
+	public ArraysFunction() {
+		
+		arr = new int[5] ; 
+	}
+	
+	
+	private static int  count  = 0 ; 
+	
+	// now using the set function set the values 
+	
+	public void set(int num) {
+		
+		// first check the size 
+		
+		if (count == arr.length) {
+			size() ; 
+		}else {
+           arr[count] = num ; 
+           count++ ; 
+		}
+		 
+		
+	}
+
+	private void size() {
+		
+		
+		// creating the size of double and double 
+		int[]copyarr = new int[arr.length]  ; 
+		
+		// creating the for loop for the 
+		for (int i = 0; i < copyarr.length; i++) {
+			copyarr[i] = arr[i] ; 
+			
+		}
+		
+		// now arrays 
+		arr = new int[arr.length*2] ; 
+		
+		
+	}
+	
     
 }
