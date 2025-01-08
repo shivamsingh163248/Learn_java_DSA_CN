e09b40f6ccda406490ccb650c965ddd097ce82e2
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-08 21:20:05 +0530

    updating and learning the auto boxing

---
+++ GreekOfGrekJavaCourse/BasricDataTypeVariable/javaAutoBoxing.java
@@ -0,0 +1,5 @@
+package BasricDataTypeVariable;
+
+public class javaAutoBoxing {
+
+}
---
+++ GreekOfGrekJavaCourse/BasricDataTypeVariable/javaUnboxing.java
@@ -0,0 +1,10 @@
+package BasricDataTypeVariable;
+
+public class javaUnboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
