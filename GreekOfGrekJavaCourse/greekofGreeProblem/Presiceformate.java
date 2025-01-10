cac598ef9a18e6a07f1e1cb96e6afd75379259a2
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-10 22:03:25 +0530

    learning the problem

---
+++ GreekOfGrekJavaCourse/greekofGreeProblem/Presiceformate.java
@@ -0,0 +1,16 @@
+package greekofGreeProblem;
+
+import java.util.ArrayList;
+
+public class Presiceformate {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Float> divisionWithPrecision(float a, float b) {
+	        // creating the arrays lsit 
+	         ArrayList<Float>value = new A
+	    }
+
+	}
+
+}
