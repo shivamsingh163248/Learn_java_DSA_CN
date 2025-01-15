50c5281a414bca39d7f89734ec77f19941213284
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-16 01:57:34 +0530

    updating the function

--- GreekOfGrekJavaCourse/greekofGreeProblem/Equilibrium_Point.java
+++ GreekOfGrekJavaCourse/greekofGreeProblem/Equilibrium_Point.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Equilibrium_Point {
 	
 	// creating the function take input as the arrys according to index 
-	public static arr[] takeinputArray() {
+	public static int[] takeinputArray() {
 		
 		// taking the input size 
 		// taking the input from the user 
@@ -16,10 +16,48 @@ public class Equilibrium_Point {
 		int[]arr = new int[n] ; 
 		
 		// creating the loop of the taking the input 
+		for (int i = 0; i < arr.length; i++) {
+			// taking input 
+			int num = input.nextInt() ; 
+			arr[i] = num ; 
+		}
 		
+		return arr ; 
+	}
+	
+	
+	// creating the function taking the 
+	public static int EqulibirumPoin(int[]arr) {
+		
+		int n = arr.length ; 
+		// creating the arr
+		int[]prifixsum  = new int[arr.length];
+		int[]postfixSum = new int[arr.length] ; 
+		
+		
+		prifixsum[0] = arr[0] ; 
+		postfixSum[n-1] = arr[n-1] ; 
+		
+		
+		
+		// now creating the loop for the sum of the number stored in the arrys 
+		for (int i = 1; i < arr.length; i++) {
+			
+			
+			prifixsum[i] = prifixsum[i-1]+arr[i] ; 
+			
+		}
+		
+		//again creating the for loop 
+		for (int i = n-2; i >= 0 ; i--) {
+			
+			// now stored the values 
+		}
 		
 		
 	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
