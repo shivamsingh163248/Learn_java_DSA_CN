dbc2321a049c745ac012755eb3e827128157a6d8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-15 23:18:00 +0530

    creating the taking input function form the user

--- GreekOfGrekJavaCourse/greekofGreeProblem/Equilibrium_Point.java
+++ GreekOfGrekJavaCourse/greekofGreeProblem/Equilibrium_Point.java
@@ -1,11 +1,22 @@
 package greekofGreeProblem;
 
+import java.util.Scanner;
+
 public class Equilibrium_Point {
 	
 	// creating the function take input as the arrys according to index 
 	public static arr[] takeinputArray() {
 		
 		// taking the input size 
+		// taking the input from the user 
+		Scanner input = new Scanner(System.in) ;
+		System.out.println("Enter the size of the arrys ");
+		int n = input.nextInt() ; 
+		
+		int[]arr = new int[n] ; 
+		
+		// creating the loop of the taking the input 
+		
 		
 		
 	}
