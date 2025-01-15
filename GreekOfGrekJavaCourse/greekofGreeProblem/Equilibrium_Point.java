642f747c0055b6a55f59da36cf45e974fb044add
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-15 21:55:58 +0530

    update

--- GreekOfGrekJavaCourse/greekofGreeProblem/Equilibrium_Point.java
+++ GreekOfGrekJavaCourse/greekofGreeProblem/Equilibrium_Point.java
@@ -1,6 +1,14 @@
 package greekofGreeProblem;
 
 public class Equilibrium_Point {
+	
+	// creating the function take input as the arrys according to index 
+	public static arr[] takeinputArray() {
+		
+		// taking the input size 
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
