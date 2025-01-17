f894299740619c24223d0853e727073d91c45991
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-17 14:16:16 +0530

    update

---
+++ GreekOfGrekJavaCourse/greekofGreeProblem/LongestSubArryasSumk.java
@@ -0,0 +1,18 @@
+package greekofGreeProblem;
+
+public class LongestSubArryasSumk {
+	
+	// using the hash map we solved the problem 
+	// creating the function of the hasMap to solved the problem 
+	public static int LongestSubArray(int[]arr , int k ) {
+		
+		// creating the hasMap which have the key and values
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
