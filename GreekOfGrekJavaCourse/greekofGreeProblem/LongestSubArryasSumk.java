40fb677a4cc5e2d3fada40a49e9c0bda407e33be
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-18 21:55:24 +0530

    update

--- GreekOfGrekJavaCourse/greekofGreeProblem/LongestSubArryasSumk.java
+++ GreekOfGrekJavaCourse/greekofGreeProblem/LongestSubArryasSumk.java
@@ -1,5 +1,7 @@
 package greekofGreeProblem;
 
+import java.util.HashMap;
+
 public class LongestSubArryasSumk {
 	
 	// using the hash map we solved the problem 
@@ -7,6 +9,25 @@ public class LongestSubArryasSumk {
 	public static int LongestSubArray(int[]arr , int k ) {
 		
 		// creating the hasMap which have the key and values
+		// hasMap is the generic 
+		int prefixSum = 0 ; 
+		int res = 0 ; 
+		
+		
+		HashMap<Integer, Integer>map = new HashMap<>() ;
+		// hasMap the property to set the values 
+		// creating the loop 
+		for (int i = 0; i < arr.length; i++) {
+			
+			// creating the store the value in the hasMap
+			
+			prefixSum += arr[i]  ; // hear we are the storing the value 
+			
+			
+			if ( == k) {
+				
+			}
+		}
 		
 	}
 
