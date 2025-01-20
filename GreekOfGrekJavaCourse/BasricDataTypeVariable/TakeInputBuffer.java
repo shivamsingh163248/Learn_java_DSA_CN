fcac7d932d7cfd777f72f94c984627a2e38094b3
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-21 01:05:39 +0530

    updating the function

--- GreekOfGrekJavaCourse/BasricDataTypeVariable/TakeInputBuffer.java
+++ GreekOfGrekJavaCourse/BasricDataTypeVariable/TakeInputBuffer.java
@@ -14,12 +14,20 @@ public class TakeInputBuffer {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)) ; 
 		
 		// agin creating the new object 
-		System.out.println("enter the number ");
-		String n  = br.readLine() ; 
+		System.out.println("enter the name  ");
+		String n  = br.readLine() ;
+		
+		// taking the input as the int 
+		System.out.println("enter the age");
+		int number = Integer.parseInt(  br.readLine()) ; 
+		System.out.println(n+" "+number);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		// calling the function 
+		
+		printUsingBufferReader();
 
 	}
 
