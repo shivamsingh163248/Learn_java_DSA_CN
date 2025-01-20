09f0e175868cec8f6a1ed075f30d5315725cad3e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-20 21:39:01 +0530

    update

---
+++ GreekOfGrekJavaCourse/greekofGreeProblem/LargestSubArrysZeroAndOnce.java
@@ -0,0 +1,32 @@
+package greekofGreeProblem;
+
+// creating the function of the longest sum arrys 
+// creating the static function 
+
+public class LargestSubArrysZeroAndOnce {
+	
+	// creating the function 
+	public static int longestArr(int arr[]) {
+		
+		int sum = 0 ; 
+		int LongestArrys = 0  ; 
+		
+		// creating the condition 
+	    // creating the loop 
+		for (int i = 0; i < arr.length; i++) {
+			
+			// creating a sum 
+			sum += (arr[i] == 0 )? 1:-1 ; 
+			
+			
+			
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
