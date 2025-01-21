3bd99e7ab4f09ea1b4b79ee3fffff7c490672c92
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-21 23:16:46 +0530

    learning the string

---
+++ GreekOfGrekJavaCourse/BasricDataTypeVariable/OutPut.java
@@ -0,0 +1,18 @@
+package BasricDataTypeVariable;
+
+public class OutPut {
+	
+	// Creating the function 
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "sh" ;  ; 
+		String b = " name" ; 
+		
+		
+		System.out.println(a+b);
+
+	}
+
+}
