23ea70eb1ac7ca08bb626c2984068d1cad3ca78f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-23 14:01:18 +0530

    updating

--- GreekOfGrekJavaCourse/greekofGreeProblem/ArraysPuzzleProblem.java
+++ GreekOfGrekJavaCourse/greekofGreeProblem/ArraysPuzzleProblem.java
@@ -26,6 +26,35 @@ public class ArraysPuzzleProblem {
 		
 	}
 
+	// now writing the otmized code 
+	
+	public static int[] puzzel(int[]arr) {
+		
+		// creating the new arrays 
+		int[]reult = new int[arr.length] ; 
+		
+		// creating the all the sum 
+		// creating the loop 
+		int product =1 ; 
+		
+	
+		
+		// creating the double loop 
+		for (int i = 0; i < reult.length; i++) {
+			product *= arr[i] ; 
+		}
+		for (int j = 0; j < reult.length; j++) {
+		    reult[j] = product / arr[j] ; 
+		}
+		
+		return reult ; 
+		
+		// this found the error 
+		
+	}
+	
+	// creating the another approch 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
