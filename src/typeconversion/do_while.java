ae455eabdc7cccacff6bde6184b520c92bcccc47
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-25 15:05:17 +0530

    updating problem

---
+++ src/typeconversion/do_while.java
@@ -0,0 +1,24 @@
+package typeconversion;
+
+public class do_while {
+	
+	// creating the static function of the do while loop 
+	public static void dowhile() {
+		
+		// creating the loop 
+		int x = 0 ; 
+		do {
+			System.out.println(x);
+			x++ ; 
+			
+		} while (5>x);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		// calling the functon 
+		dowhile();
+	}
+
+}
