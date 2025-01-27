7659fa2b8f2166895e7af57aae80f1b29b4453e9
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-01-28 00:11:17 +0530

    update

---
+++ GreekOfGrekJavaCourse/LinkList/Node.java
@@ -0,0 +1,16 @@
+package LinkList;
+
+public class Node {
+	
+	// creating a data 
+	int Data ; 
+	Node next ; 
+	
+	// creating counstocture 
+	public Node(int data) {
+		this.Data = data ; 
+		
+	}
+	
+
+}
